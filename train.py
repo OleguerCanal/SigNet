aead7efb55af39e52054d51a8403162acdc01974
cserranocolome <claudiaserranocolome@gmail.com>
2021-03-24 12:31:14 +0100

    Added parameters of the scheduled learning rate

--- train.py
+++ train.py
@@ -14,10 +14,12 @@ from utilities import plot_signature, get_data_batches, get_entropy
 num_hidden_layers = 4
 num_neurons = 500
 num_classes = 4
-learning_rate_gamma = 0.9
+intial_learning_rate = 0.0001
+learning_rate_steps = 100
+learning_rate_gamma = 0.1
 
 # Training params
-experiment_id = "test_0"
+experiment_id = "test_141"
 iterations = 1e3
 batch_size = 50
 
@@ -31,8 +33,8 @@ if __name__ == "__main__":
     writer = SummaryWriter(log_dir=os.path.join("runs", experiment_id))
 
     sn = SignatureNet(num_classes=num_classes, num_hidden_layers=num_hidden_layers, num_units=num_neurons)
-    optimizer = optim.Adam(sn.parameters())
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=100, gamma=learning_rate_gamma)
+    optimizer = optim.Adam(sn.parameters(), lr = intial_learning_rate)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=learning_rate_steps, gamma=learning_rate_gamma)
     loss = nn.CrossEntropyLoss()
 
     for iteration in tqdm(range(int(iterations))):
@@ -52,6 +54,7 @@ if __name__ == "__main__":
         l.backward()
         optimizer.step()
         scheduler.step()
+        #print(scheduler.get_lr())
 
     torch.save(sn.state_dict(), os.path.join("models", experiment_id))
 
