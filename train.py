5156e362d0f291b7daee9a17b5a74f719d71c6ad
OleguerCanal <oleguer.canal@hotmail.com>
2021-03-24 10:38:15 +0100

    Using crossentropy loss

--- train.py
+++ train.py
@@ -8,21 +8,30 @@ from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
 from model import SignatureNet
-from utilities import plot_signature, get_data_batches
+from utilities import plot_signature, get_data_batches, get_entropy
 
-experiment_id = "test_2"
-iterations = 1e5
-batch_size = 1000
+# Model params
+num_hidden_layers = 4
+num_neurons = 300
+num_classes = 4
+
+# Training params
+experiment_id = "test_9"
+iterations = 1e6
+batch_size = 50
 
 if __name__ == "__main__":
     data = pd.read_excel("data.xlsx")
     signatures = [torch.tensor(data.iloc[:,i]).type(torch.float32) for i in range(2, 74)]
 
+    # TODO: Remove this line
+    signatures = signatures[:num_classes]  # Classify only first 5 signatures
+
     writer = SummaryWriter(log_dir=os.path.join("runs", experiment_id))
 
-    sn = SignatureNet()
-    optimizer = optim.Adam(sn.parameters(), lr=0.01)
-    loss = nn.MSELoss()
+    sn = SignatureNet(num_classes=num_classes, num_hidden_layers=num_hidden_layers, num_units=num_neurons)
+    optimizer = optim.Adam(sn.parameters(), lr=0.0001)
+    loss = nn.CrossEntropyLoss()
 
     for iteration in tqdm(range(int(iterations))):
         input_batch, label_batch = get_data_batches(signatures=signatures,
@@ -31,9 +40,12 @@ if __name__ == "__main__":
         optimizer.zero_grad()
 
         predicted_batch = sn(input_batch)
+        # print(predicted_batch)
+        # print(label_batch)
         l = loss(predicted_batch, label_batch)
 
         writer.add_scalar(f'loss', l.item(), iteration)
+        writer.add_scalar(f'entropy', get_entropy(predicted_batch), iteration)
 
         l.backward()
         optimizer.step()
