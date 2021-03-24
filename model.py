1c39f18fa76974431d3c19b36ee5f787ba8a866c
OleguerCanal <oleguer.canal@hotmail.com>
2021-03-24 09:38:07 +0100

    Number of layers and hidden neurons as arg

--- model.py
+++ model.py
@@ -3,19 +3,19 @@ import torch.nn as nn
 
 class SignatureNet(nn.Module):
 
-    def __init__(self):
+    def __init__(self, num_hidden_layers=2, num_units=400):
         super(SignatureNet, self).__init__()
         
-        self.fc1 = nn.Linear(96, 500)
-        self.fc2 = nn.Linear(500, 500)
-        self.fc3 = nn.Linear(500, 72)
+        self.layers = [nn.Linear(96, num_units)]
+        for _ in range(num_hidden_layers):
+            self.layers.append(nn.Linear(num_units, num_units))
+        self.layers.append(nn.Linear(num_units, 72))
 
-        self.activation = torch.sigmoid
+        self.activation = torch.ReLU()
 
 
     def forward(self, x):
-        x = self.activation(self.fc1(x))
-        x = self.activation(self.fc2(x))
-        x = self.fc3(x)
+        for layer in self.layers:
+            x = self.activation(layer(x))
         x = nn.Softmax(dim=1)(x)
         return x
