8390a0e67f5c024ca77a024a4d5acc930a4e7225
cserranocolome <claudiaserranocolome@gmail.com>
2021-03-26 15:47:17 +0100

    Loss function for soft labels

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import os
 
 import pandas as pd
+from snorkel import classification
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -15,13 +16,13 @@ num_hidden_layers = 4
 num_neurons = 500
 num_classes = 72
 intial_learning_rate = 0.0001
-learning_rate_steps = 5000
+learning_rate_steps = 20000
 learning_rate_gamma = 0.1
 
 # Training params
-experiment_id = "test_110"
-iterations = 5e3
-batch_size = 50
+experiment_id = "test_118"
+iterations = 2e4
+batch_size = 100
 num_samples = 5000
 
 if __name__ == "__main__":
@@ -36,8 +37,8 @@ if __name__ == "__main__":
     sn = SignatureNet(num_classes=num_classes, num_hidden_layers=num_hidden_layers, num_units=num_neurons)
     optimizer = optim.Adam(sn.parameters(), lr = intial_learning_rate)
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=learning_rate_steps, gamma=learning_rate_gamma)
-    loss = nn.CrossEntropyLoss()
-    
+    #loss = nn.CrossEntropyLoss()
+
     predicted_list = torch.zeros(0,dtype=torch.long)
     label_list = torch.zeros(0,dtype=torch.long)
 
@@ -53,7 +54,7 @@ if __name__ == "__main__":
             label_list = torch.cat([label_list,label_batch.view(-1)])
             predicted_list = torch.cat([predicted_list, torch.argmax(predicted_batch,1).view(-1)])
 
-        l = loss(predicted_batch, label_batch)
+        l = classification.cross_entropy_with_probs(predicted_batch, label_batch)
 
         writer.add_scalar(f'loss', l.item(), iteration)
         #writer.add_scalar(f'entropy', get_entropy(predicted_batch), iteration)
@@ -61,14 +62,17 @@ if __name__ == "__main__":
         l.backward()
         optimizer.step()
         scheduler.step()
-        #print(scheduler.get_lr())
 
     torch.save(sn.state_dict(), os.path.join("models", experiment_id))
-    conf_mat = confusion_matrix_plot(label_list, predicted_list, range(num_classes))
-
+    #conf_mat = confusion_matrix_plot(label_list, predicted_list, range(num_classes))
+    
     sm = torch.nn.Softmax()
-    for i in range(num_classes):
-        prediction = sn(signatures[i].unsqueeze(dim=0))
-        probabilities = sm(prediction)
-        print(i)
-        print(probabilities)
+    #for i in range(num_classes):
+    #    prediction = sn(signatures[i].unsqueeze(dim=0))
+    #    probabilities = sm(prediction)
+    #    print(i)
+    #    print(probabilities)
+
+    prediction = sn((signatures[2]*0.3 + signatures[5]*0.7).unsqueeze(dim=0))
+    probabilities = sm(prediction)
+    print(probabilities)
--- utilities.py
+++ utilities.py
@@ -6,6 +6,7 @@ import seaborn as sns
 from sklearn.metrics import confusion_matrix
 import torch
 
+
 def plot_signature(signature, labels):
     plt.bar(range(96), signature, tick_label=labels)
     plt.xticks(rotation=90)
@@ -13,11 +14,17 @@ def plot_signature(signature, labels):
 
 def get_data_batches(signatures, batch_size, n_samples=500):
     input_batch = torch.empty(batch_size, 96)
-    label_batch = torch.empty(batch_size, dtype=torch.long)
+    label_batch = torch.empty(batch_size, 72)
 
     for i in range(batch_size):
-        label = random.randint(0, len(signatures)-1)
-        signature = signatures[label]
+        labels = np.random.randint(0, len(signatures)-1, size = 2)
+        w = random.uniform(0, 1)
+        label_weights = np.zeros(72)
+        label_weights[labels[0]] = w
+        label_weights[labels[1]] = 1-w
+        label_weights = torch.from_numpy(label_weights) 
+
+        signature = signatures[labels[0]]*w + signatures[labels[1]]*(1-w)
         c = torch.distributions.categorical.Categorical(probs=signature)
         samples = c.sample(sample_shape=torch.Size([n_samples,])).type(torch.float32)
         sample = torch.histc(samples, bins=96, min=0, max=95)/float(n_samples)
@@ -25,7 +32,7 @@ def get_data_batches(signatures, batch_size, n_samples=500):
         # plot_signature(signature=sample.numpy(), labels=None)
         input_batch[i, :] = sample
         # label_batch[i, :] = torch.nn.functional.one_hot(torch.tensor(label),  num_classes=72)
-        label_batch[i] = torch.tensor(label).type(torch.long)
+        label_batch[i, :] = label_weights
     return input_batch, label_batch
 
 def get_entropy(predicted_batch):
