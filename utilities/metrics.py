bb105b80f891662ff3a6e1bb942a225183288321
OleguerCanal <oleguer.canal@hotmail.com>
2021-03-26 18:03:59 +0100

    Fixed bug

--- utilities/metrics.py
+++ utilities/metrics.py
@@ -3,7 +3,7 @@ import torch
 def get_cosine_similarity(predicted_batch, label_batch):
     predicted_batch = torch.nn.functional.normalize(predicted_batch, dim=1, p=2)
     label_batch = torch.nn.functional.normalize(label_batch, dim=1, p=2)
-    return torch.mean(torch.einsum("ij,ij->i", (predicted_batch, label_batch)))
+    return torch.mean(torch.abs(torch.einsum("ij,ij->i", (predicted_batch, label_batch))))
 
 def get_entropy(predicted_batch):
     batch_size = predicted_batch.shape[0]
