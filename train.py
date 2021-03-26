d23f39a68dd4c3d1a318e8ca040a1834299bff26
OleguerCanal <oleguer.canal@hotmail.com>
2021-03-26 17:51:56 +0100

    Added dataloader

--- train.py
+++ train.py
@@ -9,65 +9,73 @@ from torch.utils.tensorboard import SummaryWriter
 from tqdm import tqdm
 
 from model import SignatureNet
-from utilities import plot_signature, get_data_batches, get_entropy, confusion_matrix_plot
+from utilities.dataloader import DataLoader
+from utilities.metrics import get_cosine_similarity, get_entropy
+from utilities.plotting import plot_signature, plot_confusion_matrix
 
 # Model params
 num_hidden_layers = 4
 num_neurons = 500
 num_classes = 72
-intial_learning_rate = 0.0001
+intial_learning_rate = 0.01
 learning_rate_steps = 20000
 learning_rate_gamma = 0.1
 
 # Training params
-experiment_id = "test_118"
+experiment_id = "test_120"
 iterations = 2e4
 batch_size = 100
 num_samples = 5000
 
 if __name__ == "__main__":
     data = pd.read_excel("data.xlsx")
-    signatures = [torch.tensor(data.iloc[:,i]).type(torch.float32) for i in range(2, 74)]
+    signatures = [torch.tensor(data.iloc[:, i]).type(
+        torch.float32) for i in range(2, 74)][:num_classes]
 
-    # TODO: Remove this line
-    signatures = signatures[:num_classes]  # Classify only first 5 signatures
+    data_loader = DataLoader(signatures=signatures,
+                             batch_size=batch_size,
+                             n_samples=num_samples,
+                             min_n_signatures=1,
+                             max_n_signatures=3)
 
     writer = SummaryWriter(log_dir=os.path.join("runs", experiment_id))
 
-    sn = SignatureNet(num_classes=num_classes, num_hidden_layers=num_hidden_layers, num_units=num_neurons)
-    optimizer = optim.Adam(sn.parameters(), lr = intial_learning_rate)
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=learning_rate_steps, gamma=learning_rate_gamma)
+    sn = SignatureNet(num_classes=num_classes,
+                      num_hidden_layers=num_hidden_layers, num_units=num_neurons)
+    optimizer = optim.Adam(sn.parameters(), lr=intial_learning_rate)
+    scheduler = optim.lr_scheduler.StepLR(
+        optimizer, step_size=learning_rate_steps, gamma=learning_rate_gamma)
     #loss = nn.CrossEntropyLoss()
 
-    predicted_list = torch.zeros(0,dtype=torch.long)
-    label_list = torch.zeros(0,dtype=torch.long)
+    predicted_list = torch.zeros(0, dtype=torch.long)
+    label_list = torch.zeros(0, dtype=torch.long)
 
     for iteration in tqdm(range(int(iterations))):
-        input_batch, label_batch = get_data_batches(signatures=signatures,
-                                                    batch_size=batch_size,
-                                                    n_samples=num_samples)
+        input_batch, label_batch = data_loader.get_batch()
         optimizer.zero_grad()
 
         predicted_batch = sn(input_batch)
 
-        if iteration > iterations/2:
-            label_list = torch.cat([label_list,label_batch.view(-1)])
-            predicted_list = torch.cat([predicted_list, torch.argmax(predicted_batch,1).view(-1)])
+        # if iteration > iterations/2:
+        #     label_list = torch.cat([label_list, label_batch.view(-1)])
+        #     predicted_list = torch.cat(
+        #         [predicted_list, torch.argmax(predicted_batch, 1).view(-1)])
 
-        l = classification.cross_entropy_with_probs(predicted_batch, label_batch)
+        l = classification.cross_entropy_with_probs(
+            predicted_batch, label_batch)
 
-        writer.add_scalar(f'loss', l.item(), iteration)
-        #writer.add_scalar(f'entropy', get_entropy(predicted_batch), iteration)
+        writer.add_scalar(f'metrics/loss', l.item(), iteration)
+        writer.add_scalar(f'metrics/cosine_similarity', get_cosine_similarity(predicted_batch, label_batch), iteration)
 
         l.backward()
         optimizer.step()
         scheduler.step()
 
     torch.save(sn.state_dict(), os.path.join("models", experiment_id))
-    #conf_mat = confusion_matrix_plot(label_list, predicted_list, range(num_classes))
-    
+    #conf_mat = plot_confusion_matrix(label_list, predicted_list, range(num_classes))
+
     sm = torch.nn.Softmax()
-    #for i in range(num_classes):
+    # for i in range(num_classes):
     #    prediction = sn(signatures[i].unsqueeze(dim=0))
     #    probabilities = sm(prediction)
     #    print(i)
---
+++ utilities/dataloader.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import random
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+import torch
+
+class DataLoader:  #TODO(oleguer): Inherit from torch.utils.data.Dataset
+
+    def __init__(self, signatures, batch_size, n_samples=1000, min_n_signatures=1, max_n_signatures=10):
+        self.__total_signatures = len(signatures)
+        self.signatures = torch.stack(signatures).t()
+        self.batch_size = batch_size
+        self.n_samples = n_samples
+        assert(min_n_signatures > 0 and max_n_signatures <= self.__total_signatures)
+        self.min_n_signatures = min_n_signatures  # Inclusive
+        self.max_n_signatures = max_n_signatures  # Inclusive
+
+    def get_batch(self):
+        input_batch = torch.empty(self.batch_size, 96)
+        label_batch = torch.empty(self.batch_size, 72)
+
+        for i in range(self.batch_size):
+            # Pick the number of involved signatures
+            n_signatures = np.random.randint(self.min_n_signatures, self.max_n_signatures + 1)
+            
+            # Select n_signatures
+            signature_ids = torch.randperm(self.__total_signatures)[:n_signatures]
+            
+            # Assign weights randomly
+            weights = torch.rand(size=(n_signatures,))
+            weights = weights/torch.sum(weights)
+            label = torch.zeros(self.__total_signatures).scatter_(dim=0, index=signature_ids, src=weights)
+            
+            # Compute resulting signature
+            signature = torch.einsum("ij,j->i", (self.signatures, label))
+            
+            # Sample
+            c = torch.distributions.categorical.Categorical(probs=signature)
+            samples = c.sample(sample_shape=torch.Size([self.n_samples,])).type(torch.float32)
+            sample = torch.histc(samples, bins=96, min=0, max=95)/float(self.n_samples)
+            
+            # Store
+            input_batch[i, :] = sample
+            label_batch[i, :] = label
+        return input_batch, label_batch
