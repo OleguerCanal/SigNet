5e37c059e36f309f6fb1b7c1b50f2602dc5d375d
OleguerCanal <oleguer.canal@hotmail.com>
2021-03-28 18:44:58 +0200

    Added num_classes as param

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ class SignatureNet(nn.Module):
 
     def __init__(self, num_classes=72, num_hidden_layers=2, num_units=400):
         super(SignatureNet, self).__init__()
+        self.num_classes = num_classes
         
         self.layers = [nn.Linear(96, num_units)]
         for _ in range(num_hidden_layers):
