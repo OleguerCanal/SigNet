f21ce2b4b0e0581d6b3749ce7ceba49079a0810e
OleguerCanal <oleguer.canal@hotmail.com>
2021-03-28 21:34:05 +0200

    Autopep

--- utilities/metrics.py
+++ utilities/metrics.py
@@ -13,11 +13,13 @@ def get_cosine_similarity(predicted_batch, label_batch):
     label_batch = torch.nn.functional.normalize(label_batch, dim=1, p=2)
     return torch.mean(torch.abs(torch.einsum("ij,ij->i", (predicted_batch, label_batch))))
 
+
 def get_entropy(predicted_batch):
     batch_size = predicted_batch.shape[0]
     entropy = torch.mean(torch.distributions.categorical.Categorical(
         probs=predicted_batch.detach()).entropy())
     return entropy
+
 
 def get_cross_entropy(predicted_label, true_label):
     return classification.cross_entropy_with_probs(predicted_label, true_label)
