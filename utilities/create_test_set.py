cdfa2de1bc1383da2139716034bf98517f4cd6ee
OleguerCanal <oleguer.canal@hotmail.com>
2021-03-29 00:07:23 +0200

    Updated code to create test sets

--- utilities/create_test_set.py
+++ utilities/create_test_set.py
@@ -1,25 +1,35 @@
 import os
+import sys
 
 import numpy as np
 import pandas as pd
 import torch
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utilities.dataloader import DataLoader
+
+torch.seed = 0
+np.random.seed(0)
 
 if __name__ == "__main__":
     num_classes = 72
 
     # Generate data
-    data = pd.read_excel("data.xlsx")
+    data = pd.read_excel("../data/data.xlsx")
     signatures = [torch.tensor(data.iloc[:, i]).type(torch.float32)
                 for i in range(2, 74)][:num_classes]
     dataloader = DataLoader(signatures=signatures,
                             batch_size=500,
-                            n_samples=5000,
+                            n_samples=500,
                             min_n_signatures=1,
-                            max_n_signatures=20)
+                            max_n_signatures=15,
+                            seed=0)
     input_batch, label_batch = dataloader.get_batch(normalize=False)
     df = input_batch.detach().numpy()
     df = np.array(df, dtype=int)
     df = pd.DataFrame(df)
-    df.to_csv("test_set.csv", header=False, index=False)
+    df.to_csv("../data/test_input_2.csv", header=False, index=False)
+
+    df = label_batch.detach().numpy()
+    df = pd.DataFrame(df)
+    df.to_csv("../data/test_label_2.csv", header=False, index=False)
