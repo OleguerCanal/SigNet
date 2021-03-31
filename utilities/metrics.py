db389fd1b2a3ed61dcfd4fdb0e9e2a5a4314466c
cserranocolome <claudiaserranocolome@gmail.com>
2021-03-31 13:43:57 +0200

    Corrected jensen shannon

--- utilities/metrics.py
+++ utilities/metrics.py
@@ -2,7 +2,6 @@ import torch
 from scipy.stats import wasserstein_distance
 from snorkel import classification
 
-
 def get_MSE(predicted_label, true_label):
     return torch.nn.MSELoss()(predicted_label, true_label)
 
@@ -15,7 +14,6 @@ def get_cosine_similarity(predicted_batch, label_batch):
 
 
 def get_entropy(predicted_batch):
-    print(predicted_batch)
     batch_size = predicted_batch.shape[0]
     entropy = torch.mean(torch.distributions.categorical.Categorical(
         probs=predicted_batch.detach()).entropy())
@@ -26,15 +24,20 @@ def get_cross_entropy(predicted_label, true_label):
     return classification.cross_entropy_with_probs(predicted_label, true_label)
 
 def get_cross_entropy2(predicted_label, true_label):
-    return torch.mean(-torch.einsum("ij,ij->i",(predicted_label, torch.log(true_label))))
+    return torch.mean(-torch.einsum("ij,ij->i",(true_label, torch.log(predicted_label))))
 
 
 def get_kl_divergence(predicted_label, true_label):
-    return get_cross_entropy(predicted_label, true_label) - get_entropy(true_label)
+    return get_cross_entropy2(predicted_label, true_label) - get_entropy(true_label)
 
 
 def get_jensen_shannon(predicted_label, true_label):
-    return 0.5 * (get_kl_divergence(predicted_label, (true_label + predicted_label)*0.5) + get_kl_divergence(true_label, (true_label + predicted_label)*0.5))
+    print(predicted_label)
+    print(true_label)
+    r = (predicted_label + true_label)/2
+    term1 = torch.mean(torch.einsum("ij,ij->i",(predicted_label,torch.nan_to_num(torch.log(torch.div(predicted_label, r))))))
+    term2 = torch.mean(torch.einsum("ij,ij->i",(true_label, torch.nan_to_num(torch.log(torch.div(true_label, r))))))
+    return 0.5 * (term1 + term2) 
 
 
 def get_wasserstein_distance(predicted_label, true_label):
