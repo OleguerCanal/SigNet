6eca2346f5acce64c844fd10c490802d761deef2
cserranocolome <claudiaserranocolome@gmail.com>
2021-03-31 13:43:35 +0200

    Added regularization

--- train.py
+++ train.py
@@ -19,13 +19,14 @@ num_neurons = 600
 num_classes = 72
 
 # Training params
-experiment_id = "comb_kl_1"
+experiment_id = "comb_kl_reg2"
 iterations = 1500
 batch_size = 50
 num_samples = 1000
-intial_learning_rate = 0.001
+intial_learning_rate = 0.01
 learning_rate_steps = 300
 learning_rate_gamma = 0.7
+l1_lambda = 1e-4
 
 if __name__ == "__main__":
 
@@ -34,6 +35,10 @@ if __name__ == "__main__":
     signatures = [torch.tensor(data.iloc[:, i]).type(
         torch.float32) for i in range(2, 74)][:num_classes]
 
+    input_validation = torch.tensor(pd.read_csv("data/validation_input_2.csv").values, dtype=torch.float)
+    input_validation = torch.nn.functional.normalize(input_validation, dim=1, p=1)
+    label_validation = torch.tensor(pd.read_csv("data/validation_label_2.csv").values, dtype=torch.float)
+    
     data_loader = DataLoader(signatures=signatures,
                              batch_size=batch_size,
                              n_samples=num_samples,
@@ -41,6 +46,7 @@ if __name__ == "__main__":
                              max_n_signatures=15)
 
     writer = SummaryWriter(log_dir=os.path.join("runs", experiment_id))
+    writer_validation = SummaryWriter(log_dir=os.path.join("runs/validation", experiment_id))
 
     sn = SignatureNet(signatures=signatures,
                       num_classes=num_classes,
@@ -63,13 +69,17 @@ if __name__ == "__main__":
         # l = get_MSE(predicted_batch, label_batch)
         l = get_kl_divergence(predicted_batch, label_batch)
         # l = get_jensen_shannon(predicted_batch, label_batch)
+        l1_norm = sum(p.abs().sum() for p in sn.parameters())
+        l = l + l1_lambda*l1_norm
 
         l.backward()
         optimizer.step()
         scheduler.step()
 
+
         writer.add_scalar(f'metrics/cross-entropy', get_cross_entropy(predicted_batch, label_batch), iteration)
-        writer.add_scalar(f'metrics/cosine_similarity', get_cosine_similarity(predicted_batch, label_batch), iteration)
+        writer.add_scalar(f'metrics/cosine_similarity', get_cosine_similarity(sn(input_batch), label_batch), iteration)
+        writer_validation.add_scalar(f'metrics/cosine_similarity', get_cosine_similarity(sn(input_validation), label_validation), iteration)
         writer.add_scalar(f'metrics/KL-divergence', get_kl_divergence(predicted_batch, label_batch), iteration)
         writer.add_scalar(f'metrics/js-divergence', get_jensen_shannon(predicted_batch, label_batch), iteration)
         writer.add_scalar(f'metrics/mse', get_MSE(predicted_batch, label_batch), iteration)
