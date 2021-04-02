008cfdf865bd4fdd24b366b402b46552917809cc
cserranocolome <claudiaserranocolome@gmail.com>
2021-04-02 16:29:55 +0200

    Added global constrain

--- baseline.py
+++ baseline.py
@@ -16,6 +16,8 @@ class SignatureFinder:
     # TODO(oleguer): Use torch tensors instead of np arrays everywhere
 
     def __init__(self, signatures, metric=None, lagrange_mult=0.1):
+        global constrain
+        
         self.signatures = torch.stack(signatures).t().cpu().detach().numpy()
         self.__weight_len = self.signatures.shape[1]
         self.__bounds = [(0, 1)]*self.__weight_len
@@ -23,25 +25,26 @@ class SignatureFinder:
         self.metric = metric if metric is not None else get_MSE
         # Define sum-1 constrain
 
-        def __constrain(w):
+        def constrain(w):
             return np.sum(w) - 1
-        self.constraints = [{"type": "eq", "fun": __constrain}]
+        self.constraints = [{"type": "eq", "fun": constrain}]
 
     def __objective(self, w):
         with torch.no_grad():
             guess = np.dot(self.signatures, w)
             guess_tensor = torch.tensor(guess, dtype=torch.float).unsqueeze(0)
-            error = self.metric(self.signature_tensor, guess_tensor).item()
+            error = self.metric(guess_tensor, self.signature_tensor).item()
         # return error + self.lagrange_mult*(np.sum(w) - 1)**2
         return error
 
     def get_weights(self, signature):
+        #w = np.ones(self.__weight_len)/self.__weight_len
         w = np.random.uniform(low=0, high=1, size=(self.__weight_len,))
+        w = w/sum(w)
         self.signature_tensor = torch.tensor(signature, dtype=torch.float).unsqueeze(0)
         res = minimize(self.__objective, w,
                        bounds=self.__bounds,
-                       constraints=self.constraints
-                       )
+                       constraints=self.constraints)
         return res.x
 
     def get_weights_batch(self, input_batch):
@@ -57,20 +60,26 @@ class SignatureFinder:
 
 if __name__ == "__main__":
     num_classes = 72
-    training_data =  torch.tensor(pd.read_csv("data/validation_input.csv", header=None).values, dtype=torch.float)
-    print(training_data)
+    training_data =  torch.tensor(pd.read_csv("data/training_input.csv", header=None).values, dtype=torch.float)
+    validation_data =  torch.tensor(pd.read_csv("data/validation_input.csv", header=None).values, dtype=torch.float)
+    test_data =  torch.tensor(pd.read_csv("data/test_input.csv", header=None).values, dtype=torch.float)
     data = pd.read_excel("data/data.xlsx")
     signatures = [torch.tensor(data.iloc[:, i]).type(torch.float32)
                   for i in range(2, 74)][:num_classes]
-    sf = SignatureFinder(signatures, metric=get_jensen_shannon)
-    # signature = 0.5*sf.signatures[:, 0] + 0.3*sf.signatures[:, 1] +\
-    #     0.1*sf.signatures[:, 2] + 0.1*sf.signatures[:, 3]
-    # sol = sf.get_weights(signature)
-    # print(np.round(sol, decimals=3))
-    # print(np.sum(sol))
-    # sf = SignatureFinder(signatures)
-    
+    sf = SignatureFinder(signatures, metric=get_MSE)
+
     sol = sf.get_weights_batch(training_data)
     sol = sol.detach().numpy()
     df = pd.DataFrame(sol)
-    df.to_csv("data/validation_baseline.csv", header=False, index=False)
+    df.to_csv("data/training_baseline_2.csv", header=False, index=False)
+    print("Training done!")
+    sol = sf.get_weights_batch(validation_data)
+    sol = sol.detach().numpy()
+    df = pd.DataFrame(sol)
+    df.to_csv("data/validation_baseline_2.csv", header=False, index=False)
+    print("Validation done!")
+    sol = sf.get_weights_batch(test_data)
+    sol = sol.detach().numpy()
+    df = pd.DataFrame(sol)
+    df.to_csv("data/test_baseline_2.csv", header=False, index=False)
+    print("Test done!")
