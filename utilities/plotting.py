9344b9f1fd68e13d3f72ebd799075d08509d5da0
cserranocolome <claudiaserranocolome@gmail.com>
2021-04-09 13:48:13 +0200

    Added plotting weights comparison deconstructSigs

--- utilities/plotting.py
+++ utilities/plotting.py
@@ -50,11 +50,25 @@ def plot_weights_comparison(true_labels, guessed_labels, guessed_error, sigs_nam
     ax.set_title('Signature decomposition')
     plt.tight_layout()
     plt.show()
-    
+
+def plot_weights_comparison_deconstructSigs(true_labels, deconstructSigs_labels, guessed_labels, guessed_error, sigs_names):
+    num_classes = len(guessed_labels)
+    fig, ax = plt.subplots()
+    ax.bar(range(num_classes),guessed_labels, yerr=guessed_error, align='center', width=0.2, alpha=0.5, ecolor='black', capsize=10)
+    ax.bar(np.array(range(num_classes))+0.2, true_labels, width=0.2, align='center')
+    ax.bar(np.array(range(num_classes))-0.2,deconstructSigs_labels, width=0.2, align='center')
+    ax.set_ylabel('Weights')
+    ax.set_xticks(range(num_classes))
+    ax.set_xticklabels(sigs_names, rotation='vertical')
+    ax.set_title('Signature decomposition')
+    plt.tight_layout()
+    plt.show()
+     
 if __name__ == "__main__":
+    deconstructSigs_labels = [0.1, 0.7, 0.2]
     real_labels = [0.2, 0.5, 0.3]
     guessed_labels = [0.25, 0.6, 0.2]
     guessed_error = [0.01, 0.04, 0.001]
     sigs_names = ["SBS1", "SBS2", "SBS3"]
 
-    plot_weights_comparison(real_labels, guessed_labels, guessed_error, sigs_names)
+    plot_weights_comparison_deconstructSigs(real_labels, deconstructSigs_labels, guessed_labels, guessed_error, sigs_names)
