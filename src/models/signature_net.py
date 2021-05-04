e91acfe364008f3fe18e66b70acf54146a757ea5
OleguerCanal <oleguer.canal@hotmail.com>
2021-05-04 10:51:33 +0200

    Loading models in init

--- src/models/signature_net.py
+++ src/models/signature_net.py
@@ -10,7 +10,6 @@ from finetuner import FineTuner
 from signature_finder import SignatureFinder
 from utilities.normalize_data import create_opportunities, normalize_data
 
-
 class SignatureNet:
     def __init__(self,
                  signature_finder_params,
@@ -21,37 +20,45 @@ class SignatureNet:
                  error_finder_model_name,
                  models_path="../../trained_models"):
         self.signature_finder = SignatureFinder(**signature_finder_params)
+
+        # Instantiate finetuner and read params
         self.finetuner = FineTuner(**finetuner_params)
+        self.finetuner.load_state_dict(torch.load(os.path.join(
+            models_path, finetuner_model_name), map_location=torch.device('cpu')))
+        self.finetuner.eval()
+
+        # Instantiate errorfinder and read params
         self.error_finder = ErrorFinder(**error_learner_params)
+        self.error_finder.load_state_dict(torch.load(os.path.join(
+            models_path, error_finder_model_name), map_location=torch.device('cpu')))
+        self.error_finder.eval()
+
         self.opp = create_opportunities(path_opportunities)
-        self.finetuner_model_name = finetuner_model_name
-        self.error_finder_model_name = error_finder_model_name
         self.models_path = models_path
 
-    def __call__(self, 
-                mutation_vec):
+    def __call__(self,
+                 mutation_vec):
         """Get weights of each signature in lexicographic wrt 1-mer
 
         Args:
             mutation_vec (np.array(batch_size, 96)): Batch of mutation vectors to decompose
         """
-        # Normalize input data
-        num_mutations = np.sum(mutation_vec, axis=1)
-        normalized_mutation_vec = normalize_data(mutation_vec, self.opp) 
-        normalized_mutation_vec = normalized_mutation_vec/np.sum(normalized_mutation_vec, axis=1)
+        with torch.no_grad():
+            # Normalize input data
+            num_mutations = np.sum(mutation_vec, axis=1)
+            normalized_mutation_vec = normalize_data(mutation_vec, self.opp)
+            normalized_mutation_vec = normalized_mutation_vec / \
+                np.sum(normalized_mutation_vec, axis=1)
 
-        # Run signature_finder
-        weight_guess_0 = self.signature_finder.get_weights_batch(
-            normalized_mutation_vec)
+            # Run signature_finder
+            weight_guess_0 = self.signature_finder.get_weights_batch(
+                normalized_mutation_vec)
 
-        # Run finetuner
-        self.finetuner.load_state_dict(torch.load(os.path.join(self.models_path, self.finetuner_model_name), map_location=torch.device('cpu')))
-        self.finetuner.eval()
-        weight_guess_1 = self.finetuner(mutation_dist=normalized_mutation_vec,
-                                           weights=weight_guess_0)
+            # Run finetuner
+            weight_guess_1 = self.finetuner(mutation_dist=normalized_mutation_vec,
+                                            weights=weight_guess_0)
 
-        # Run error_finder
-        self.error_finder.load_state_dict(torch.load(os.path.join(self.models_path, self.error_finder_model_name), map_location=torch.device('cpu')))
-        self.error_finder.eval()
-        positive_errors, negative_errors = self.error_finder(weights=weight_guess_1,
-                                                             num_mutations=num_mutations)
+            # Run error_finder
+            positive_errors, negative_errors = self.error_finder(weights=weight_guess_1,
+                                                                num_mutations=num_mutations)
+        return weight_guess_1, positive_errors
