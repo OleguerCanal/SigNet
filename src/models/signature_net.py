242efa22295041ac2f655ae2d69cc8c2b8a8cb6a
cserranocolome <claudiaserranocolome@gmail.com>
2021-05-04 10:39:13 +0200

    Added changes in pull request

--- src/models/signature_net.py
+++ src/models/signature_net.py
@@ -15,26 +15,29 @@ class SignatureNet:
     def __init__(self,
                  signature_finder_params,
                  finetuner_params,
-                 error_learner_params):
+                 error_learner_params,
+                 path_opportunities,
+                 finetuner_model_name,
+                 error_finder_model_name,
+                 models_path="../../trained_models"):
         self.signature_finder = SignatureFinder(**signature_finder_params)
         self.finetuner = FineTuner(**finetuner_params)
         self.error_finder = ErrorFinder(**error_learner_params)
+        self.opp = create_opportunities(path_opportunities)
+        self.finetuner_model_name = finetuner_model_name
+        self.error_finder_model_name = error_finder_model_name
+        self.models_path = models_path
 
     def __call__(self, 
-                mutation_vec, 
-                path_opportunities,
-                experiment_id_finetune,
-                experiment_id_error_finder):
+                mutation_vec):
         """Get weights of each signature in lexicographic wrt 1-mer
 
         Args:
             mutation_vec (np.array(batch_size, 96)): Batch of mutation vectors to decompose
-            path_opportunities (string): path to the txt file where the opportunities are. 
         """
         # Normalize input data
         num_mutations = np.sum(mutation_vec, axis=1)
-        opp = create_opportunities(path_opportunities)
-        normalized_mutation_vec = normalize_data(mutation_vec, opp) 
+        normalized_mutation_vec = normalize_data(mutation_vec, self.opp) 
         normalized_mutation_vec = normalized_mutation_vec/np.sum(normalized_mutation_vec, axis=1)
 
         # Run signature_finder
@@ -42,13 +45,13 @@ class SignatureNet:
             normalized_mutation_vec)
 
         # Run finetuner
-        self.finetuner.load_state_dict(torch.load(os.path.join("../../trained_models", experiment_id_finetune), map_location=torch.device('cpu')))
+        self.finetuner.load_state_dict(torch.load(os.path.join(self.models_path, self.finetuner_model_name), map_location=torch.device('cpu')))
         self.finetuner.eval()
         weight_guess_1 = self.finetuner(mutation_dist=normalized_mutation_vec,
                                            weights=weight_guess_0)
 
         # Run error_finder
-        self.error_finder.load_state_dict(torch.load(os.path.join("../../trained_models", experiment_id_error_finder), map_location=torch.device('cpu')))
+        self.error_finder.load_state_dict(torch.load(os.path.join(self.models_path, self.error_finder_model_name), map_location=torch.device('cpu')))
         self.error_finder.eval()
         positive_errors, negative_errors = self.error_finder(weights=weight_guess_1,
                                                              num_mutations=num_mutations)
