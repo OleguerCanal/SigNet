217ef8e3a586d16e0249816122aed13aa324d1cf
cserranocolome <claudiaserranocolome@gmail.com>
2021-05-04 14:40:15 +0200

    Fixed memory leak in optimize errorfinder

--- src/hyperparam_optimizers/optimize_errorfinder.py
+++ src/hyperparam_optimizers/optimize_errorfinder.py
@@ -10,12 +10,12 @@ from models.finetuner import FineTuner
 from trainers.error_trainer import ErrorTrainer
 from utilities.io import read_data
 
-experiment_id = "error_learner_0"
+experiment_id = "error_learner"
 iterations = 5
 num_classes = 72
 
 batch_sizes = Integer(name='batch_size', low=50, high=1000)
-learning_rates = Real(name='lr', low=0.00001, high=0.005)
+learning_rates = Real(name='lr', low=0.00001, high=0.05)
 neurons_pos = Integer(name='num_neurons_pos', low=20, high=1500)
 layers_pos = Integer(name='num_hidden_layers_pos', low=1, high=10)
 neurons_neg = Integer(name='num_neurons_neg', low=20, high=1500)
@@ -26,14 +26,13 @@ input_file = None
 output_file = "search_results_" + experiment_id + ".csv"
 
 # Finetuner params
-finetuner_model_name = "test_0"
+finetuner_model_name = "finetuner_model_2"
 num_hidden_layers = 1
 num_units = 1500
 model_path = "../../trained_models"
 
 if __name__ == "__main__":
     dev = "cuda" if torch.cuda.is_available() else "cpu"
-    dev = "cpu"
     device = torch.device(dev)
     print("Using device:", dev)
 
@@ -42,18 +41,29 @@ if __name__ == "__main__":
     finetuner = FineTuner(num_classes=72,
                           num_hidden_layers=num_hidden_layers,
                           num_units=num_units)
-    finetuner.to(device)
     finetuner.load_state_dict(torch.load(os.path.join(model_path, finetuner_model_name)))
+    finetuner.to("cpu")
     finetuner.eval()
 
-    train_guess_1 = finetuner(mutation_dist=train_input,
-                              weights=train_guess_0)
+    with torch.no_grad():
+        train_guess_1 = finetuner(mutation_dist=train_input,
+                                weights=train_guess_0)
 
-    val_guess_1 = finetuner(mutation_dist=val_input,
-                            weights=val_guess_0)
+        val_guess_1 = finetuner(mutation_dist=val_input,
+                                weights=val_guess_0)
+
     del finetuner
     del train_guess_0
     del val_guess_0
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    train_input = train_input.to(device)
+    train_guess_1 = train_guess_1.to(device)
+    train_label = train_label.to(device)
+    val_input = val_input.to(device)
+    val_guess_1 = val_guess_1.to(device)
+    val_label = val_label.to(device)
 
     trainer = ErrorTrainer(iterations=iterations,  # Passes through all dataset
                                train_input=train_input,
@@ -64,7 +74,8 @@ if __name__ == "__main__":
                                val_label=val_label,
                                experiment_id=experiment_id,
                                num_classes=num_classes,
-                               device=device)
+                               device=device,
+                               model_path=model_path)
 
 
     search_space = [batch_sizes, learning_rates, neurons_pos,
