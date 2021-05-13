d70ddb24e92d91d1fca9b3ca4095dc07d4759fa2
OleguerCanal <oleguer.canal@hotmail.com>
2021-05-13 15:08:22 +0200

    Logging in_prop, pi_width

--- src/loggers/error_finder_logger.py
+++ src/loggers/error_finder_logger.py
@@ -13,8 +13,16 @@ class ErrorFinderLogger:
 
     def log(self,
             train_loss,
+            train_in_prop,
+            train_pi_width,
             val_loss,
+            val_in_prop,
+            val_pi_width,
             step):
 
         self.writer.add_scalar("metrics/Loss", train_loss.item(), step)
+        self.writer.add_scalar("metrics/in_prop", train_in_prop.item(), step)
+        self.writer.add_scalar("metrics/pi_width", train_pi_width.item(), step)
         self.val_writer.add_scalar("metrics/Loss", val_loss.item(), step)
+        self.val_writer.add_scalar("metrics/in_prop", val_in_prop.item(), step)
+        self.val_writer.add_scalar("metrics/pi_width", val_pi_width.item(), step)
