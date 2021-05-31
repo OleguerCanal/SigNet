e4087acc278ba08c36659f5a7b795a13b9ebed1b
cserranocolome <claudiaserranocolome@gmail.com>
2021-05-31 10:42:05 +0200

    Add list to compare all methods

--- src/test_methods.py
+++ src/test_methods.py
@@ -10,24 +10,28 @@ from tests.model_tester import ModelTester
 data_folder = "../data/other_methods/"
 
 if __name__=="__main__":
-    inferred_results_file = "inferred.exposures_YAPSA_all_sigs.csv"
-    inferred = pd.read_csv(os.path.join(data_folder, inferred_results_file), index_col=0)
+    list_of_methods = ["decompTumor2Sig_51", "MutationalPatterns_51",  "SignatureEstimationQP_51", "YAPSA_51"]
+    #list_of_methods = ["decompTumor2sig", "deconstructSigs", "MutationalPatterns", "sigfit", "signature_estimationQP", "signature_estimationSA", "YAPSA"]
+    for method in list_of_methods:
+        inferred_results_file = "inferred.exposures_%s.csv"%method
+        inferred = pd.read_csv(os.path.join(data_folder, inferred_results_file), index_col=0)
 
-    # Create dataframe filled with 0's
-    real = pd.DataFrame(0, index=inferred.index.values.tolist(), columns=inferred.columns.values.tolist())
+        # Create dataframe filled with 0's
+        real = pd.DataFrame(0, index=inferred.index.values.tolist(), columns=inferred.columns.values.tolist())
 
-    # Update rows that have signature
-    real_different_zero = pd.read_csv(os.path.join(data_folder, "ground.truth.syn.exposures.csv"), index_col=0)
-    real.update(real_different_zero)
+        # Update rows that have signature
+        real_different_zero = pd.read_csv(os.path.join(data_folder, "ground.truth.syn.exposures_test.csv"), index_col=0)
+        real.update(real_different_zero)
 
-    # To tensor
-    inferred_tensor = torch.transpose(torch.from_numpy(np.array(inferred.values, dtype=np.float)), 0, 1)
-    real_tensor = torch.transpose(torch.from_numpy(np.array(real.values, dtype=np.float)), 0, 1)
+        # To tensor
+        inferred_tensor = torch.transpose(torch.from_numpy(np.array(inferred.values, dtype=np.float)), 0, 1)
+        real_tensor = torch.transpose(torch.from_numpy(np.array(real.values, dtype=np.float)), 0, 1)
 
-    # Normalize
-    inferred_tensor = inferred_tensor / torch.sum(inferred_tensor, dim=1).reshape(-1,1)
-    real_tensor = real_tensor / torch.sum(real_tensor, dim=1).reshape(-1,1)
+        # Normalize
+        inferred_tensor = inferred_tensor / torch.sum(inferred_tensor, dim=1).reshape(-1,1)
+        real_tensor = real_tensor / torch.sum(real_tensor, dim=1).reshape(-1,1)
 
-    model_tester = ModelTester(num_classes=72)
-    model_tester.test(guessed_labels=inferred_tensor, true_labels=real_tensor)
+        model_tester = ModelTester(num_classes=51)
+        print(method)
+        model_tester.test(guessed_labels=inferred_tensor, true_labels=real_tensor)
 
