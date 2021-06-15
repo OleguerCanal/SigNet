7581ecde139b217fcaffd468e6f33ac371790587
cserranocolome <claudiaserranocolome@gmail.com>
2021-06-15 16:31:11 +0200

    ReLU activation function

--- src/utilities/metrics.py
+++ src/utilities/metrics.py
@@ -66,12 +66,12 @@ def get_fp_fn(label_batch, prediction_batch, cutoff=0.05, num_classes=72):
     fn = torch.sum(label_mask - prediction_mask > 0)
     return fp, fn
 
-def distance_to_interval(label, pred_lower, pred_upper, lagrange_mult=1.0):
+def distance_to_interval(label, pred_lower, pred_upper, lagrange_mult=5.0):
     batch_size = float(pred_lower.shape[0])
     lower = label - pred_lower
     upper = pred_upper - label
-    lower = nn.ELU(0.1)(-lower)
-    upper = nn.ELU(0.1)(-upper)
+    lower = nn.ReLU()(-lower)
+    upper = nn.ReLU()(-upper)
     interval_length = ((pred_upper - pred_lower)**2)/batch_size
     with torch.no_grad():
         in_prop, mean_interval_width = get_pi_metrics(label, pred_lower, pred_upper)
