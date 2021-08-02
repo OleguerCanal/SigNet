8e9dcfe22d43c29473b8b7c1f30d0f06576ceb9c
OleguerCanal <oleguer.canal@hotmail.com>
2021-08-02 08:31:51 +0200

    Added scirpt to assess classification metrics of different methods

---
+++ src/classification_metrics_test.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+import pandas as pd
+import torch
+import numpy as np
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from utilities.io import read_data, read_data_yapsa
+from utilities.metrics import get_classification_metrics
+
+dev = torch.device("cpu")
+_, _, _, _, val_guess_0, val_label = read_data(dev)
+
+metrics = get_classification_metrics(label_batch=val_label[..., :-1], prediction_batch=val_guess_0)
+
+for metric in metrics:
+    print(metric, np.round(metrics[metric].detach().numpy(), 8))
