0ecc3e2c45f97476c2d925e1342e1d54434b89f7
cserranocolome <claudiaserranocolome@gmail.com>
2021-08-31 17:21:04 +0200

    Added read_data_realistic_yapsa and read_data_random_yapsa

--- src/utilities/io.py
+++ src/utilities/io.py
@@ -23,7 +23,7 @@ def read_data(device, data_folder="../data"):
     val_label = val_label.to(device)
     return train_input, train_guess_0, train_label, val_input, val_guess_0, val_label
 
-def read_data_yapsa(device, data_folder="../data"):
+def read_data_realistic_yapsa(device, data_folder="../data"):
     train_input = torch.tensor(pd.read_csv(
         data_folder + "/realistic_data/train_more_sigs/larger_realistic_train_input.csv", header=None).values, dtype=torch.float)
     train_input = train_input.to(device)
@@ -44,6 +44,29 @@ def read_data_yapsa(device, data_folder="../data"):
         data_folder + "/validation_label_w01.csv", header=None).values, dtype=torch.float)
     val_label = val_label.to(device)
     return train_input, train_guess_0, train_label, val_input, val_guess_0, val_label
+
+def read_data_random_yapsa(device, data_folder="../data"):
+    train_input = torch.tensor(pd.read_csv(
+        data_folder + "/train_input_w01.csv", header=None).values, dtype=torch.float)
+    train_input = train_input.to(device)
+    train_guess_0 = torch.tensor(pd.read_csv(
+        data_folder + "/train_baseline_yapsa.csv", header=None).values, dtype=torch.float)
+    train_guess_0 = train_guess_0.to(device)
+    train_label = torch.tensor(pd.read_csv(
+        data_folder + "/train_label_w01.csv", header=None).values, dtype=torch.float)
+    train_label = train_label.to(device)
+
+    val_input = torch.tensor(pd.read_csv(
+        data_folder + "/validation_input_w01.csv", header=None).values, dtype=torch.float)
+    val_input = val_input.to(device)
+    val_guess_0 = torch.tensor(pd.read_csv(
+        data_folder + "/validation_baseline_yapsa.csv", header=None).values, dtype=torch.float)
+    val_guess_0 = val_guess_0.to(device)
+    val_label = torch.tensor(pd.read_csv(
+        data_folder + "/validation_label_w01.csv", header=None).values, dtype=torch.float)
+    val_label = val_label.to(device)
+    return train_input, train_guess_0, train_label, val_input, val_guess_0, val_label
+
 
 def read_methods_random_data(device, data_folder="../data"):
     label = torch.tensor(pd.read_csv(
