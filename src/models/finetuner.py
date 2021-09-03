e99395ec5f44aeb24dd6f832e29bb195cf119e59
cserranocolome <claudiaserranocolome@gmail.com>
2021-09-03 10:46:08 +0200

    Added number of mutations path

--- src/models/finetuner.py
+++ src/models/finetuner.py
@@ -11,15 +11,23 @@ class FineTuner(nn.Module):
         super(FineTuner, self).__init__()
         self._cutoff = cutoff
 
-        num_units = int(num_units/2)*2  # To have an even number of units
-        num_units_branch = int(num_units/2)
+        # Num units of the mutations path
+        num_units_branch_mut = int(num_units*0.1)
 
-        self.layer1_1 = nn.Linear(num_classes, num_units_branch)
+        # Num units of the other paths
+        num_units_other_branches = num_units - num_units_branch_mut
+
+        num_units_other_branches = int(num_units_other_branches/2)*2  # To have an even number of units
+        num_units_branch = int(num_units_other_branches/2)
+
+        self.layer1_1 = nn.Linear(num_classes, num_units_branch)    # Baseline guess path
         # 96 = total number of possible muts
-        self.layer1_2 = nn.Linear(96, num_units_branch)
+        self.layer1_2 = nn.Linear(96, num_units_branch)             # Input path
+        self.layer1_3 = nn.Linear(1, num_units_branch_mut)          # Number of mutations path
 
         self.layer2_1 = nn.Linear(num_units_branch, num_units_branch)
         self.layer2_2 = nn.Linear(num_units_branch, num_units_branch)
+        self.layer2_3 = nn.Linear(num_units_branch_mut, num_units_branch_mut)
 
         self.hidden_layers = nn.ModuleList(
             modules=[nn.Linear(num_units, num_units)
@@ -33,7 +41,8 @@ class FineTuner(nn.Module):
 
     def forward(self,
                 mutation_dist,
-                weights):
+                weights,
+                num_mut):
         # Input head
         mutation_dist = self.activation(self.layer1_2(mutation_dist))
         mutation_dist = self.dropout(mutation_dist)
@@ -46,8 +55,14 @@ class FineTuner(nn.Module):
         weights = self.activation(self.layer2_1(weights))
         weights = self.dropout(weights)
 
+        # Number of mutations head
+        num_mut = self.activation(self.layer1_3(num_mut))
+        num_mut = self.dropout(num_mut)
+        num_mut = self.activation(self.layer2_3(num_mut))
+        num_mut = self.dropout(num_mut)
+
         # Concatenate
-        comb = torch.cat([mutation_dist, weights], dim=1)
+        comb = torch.cat([mutation_dist, weights, num_mut], dim=1)
 
         # Apply shared layers
         for layer in self.hidden_layers:
