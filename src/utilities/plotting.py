71aff77868ccd308bef0b7a3b3225add2e395d2c
Claudia Serrano Colomé <claudiaserranocolome@gmail.com>
2021-09-07 09:12:17 +0200

    Update src/utilities/plotting.py
    Co-authored-by: Oleguer <31748781+OleguerCanal@users.noreply.github.com>

--- src/utilities/plotting.py
+++ src/utilities/plotting.py
@@ -51,8 +51,7 @@ def plot_metric_vs_sigs(list_of_metrics, list_of_methods, list_of_guesses, label
     
     num_sigs = list(range(1, 10))
     num_sigs_ind = torch.sum(label[:, :-1]>0, 1)
-    for metric in list_of_metrics:
-        m += 1
+    for m, metric in enumerate(list_of_metrics):
         values = np.zeros((len(list_of_methods), len(num_sigs)))
 
         for k in range(len(list_of_methods)):
