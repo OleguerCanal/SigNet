a85bf4d54a13a92879c48f0c8b425e6fecfd716f
OleguerCanal <oleguer.canal@hotmail.com>
2021-09-13 18:58:07 +0200

    Added generator code

---
+++ src/models/generator.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+import torch.nn as nn
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from utilities.weights_to_mutation import WeightToMutation
+
+class Generator(nn.Module):
+
+    def __init__(self, baseline, finetuner, signatures):
+        super(Generator, self).__init__()
+        self.__baseline = baseline
+        self.__finetuner = finetuner
+        self.__w2m = WeightToMutation(signatures=signatures)
+
+    def forward(self, mutation_dist, num_mut):
+        baseline_weights = self.__baseline.get_weights_batch(mutation_dist)
+        finetuned_weights = self.__finetuner(mutation_dist, baseline_weights, num_mut)
+        guessed_mutation = self.__w2m.get_mutation(finetuned_weights, noise_variance=0.0)
+        return guessed_mutation
+
---
+++ src/train_generator.py
@@ -0,0 +1,73 @@
+
+import os
+import sys
+
+import torch
+import wandb
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from models.yapsa_inspired_baseline import YapsaInspiredBaseline
+from models.finetuner import FineTuner
+from utilities.io import read_data, read_signatures
+from trainers.generator_trainer import GeneratorTrainer
+
+config = {
+    # IDs
+    "experiment_id": "exp_0",
+    "model_id": "original",
+
+    # Training params
+    "source": "random",
+    "iterations": 40,
+    "num_classes": 72,
+    "batch_size": 500,
+    "lr": 0.0001,
+
+    # Network params
+    "num_hidden_layers": 2,
+    "num_neurons": 600,
+}
+
+models_path = os.path.join("../trained_models", config["experiment_id"])
+finetuner_path = os.path.join(models_path, "finetuner_mixed_js_loss")
+generator_path = os.path.join(models_path, "generator_" + config["source"])
+
+if __name__ == "__main__":
+    # Select training device
+    # device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = "cpu"
+    print("Using device:", device)
+
+    wandb.init(project='generator',
+               entity='sig-net',
+               config=config)
+
+    # Load data
+    signatures = read_signatures("../data/data.xlsx")
+    train_data, val_data = read_data(experiment_id=config["experiment_id"],
+                                     source=config["source"],
+                                     device=device)
+    train_data.to(device=device)
+    val_data.to(device=device)
+
+    baseline = YapsaInspiredBaseline(signatures=signatures)
+
+    finetuner = FineTuner(num_hidden_layers=config["num_hidden_layers"],
+                          num_units=config["num_neurons"])
+    finetuner.load_state_dict(torch.load(
+        finetuner_path, map_location=torch.device('cpu')))
+    finetuner.to(device)
+    finetuner.eval()
+
+    trainer = GeneratorTrainer(
+        signatures=signatures,
+        baseline=baseline,
+        finetuner=finetuner,
+        iterations=config["iterations"],  # Passes through all dataset
+        train_data=train_data,
+        val_data=val_data,
+        model_path=generator_path)
+
+    min_val = trainer.objective(batch_size=config["batch_size"],
+                                lr=config["lr"],
+                                plot=True)
