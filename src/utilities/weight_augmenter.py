c9f6422fff736f59b199e7c8d4d6ab24d78b5849
OleguerCanal <oleguer.canal@hotmail.com>
2021-09-16 14:04:24 +0200

    Updated weight augmentation function to be random

--- src/utilities/weight_augmenter.py
+++ src/utilities/weight_augmenter.py
@@ -9,51 +9,62 @@ from utilities.io import read_signatures
 
 
 class WeightAugmenter:
-    def __init__(self, signatures):
-        self.signatures = torch.stack(signatures).t()
-        self._EPS = 1e-6
-    
-    def __get_distance(self, mut, weight):
-        """Return MSE: d(weight) = ||mut - signatures*weight||^2
-        """
-        return torch.norm(mut - torch.einsum("ij,j->i", self.signatures, weight), p=2)
+    def __init__(self):
+        self._cutoff = 0.05
 
-    def __dist_gradient(self, mut, weight):
-        """If using MSE we have: d(weight) = ||mut - signatures*weight||^2
-           Thus: d'(weight) = -2*(mut - signatures*weight)^T * signatures
-        """
-        first_part = -2*(mut - torch.einsum("ij,j->i", self.signatures, weight))
-        return torch.einsum("i,ij->j", first_part, self.signatures)
+    def __normalize(self, weights):
+        return weights/torch.sum(weights, axis=1).view(-1, 1)
 
-    def get_augmentations(self, mutation_vect, inferred_weight, num_augmentations=10, step_range=(0.0, 0.5)):
-        grad = self.__dist_gradient(mut=mutation_vect, weight=inferred_weight)
-        print("grad:", grad)
-        space = np.linspace(step_range[0], step_range[1], num=10)
-        augmentations = [inferred_weight - step*grad for step in space]
-        for augmentation in augmentations:
-            print(augmentation, self.__get_distance(mutation_vect, augmentation))
+    def get_reweighted_augmentations(self, weight, num_augmentations=5, augmentation_variance=0.5):
+        """Applies normal weight changes with variance augmentation_variance to the batch of weights
+        with weight higher than a minimum threshold.
 
+        Args:
+            weight (torch.tensor(batch,num_sigs])): Batch of guessed weights
+            num_augmentations (int, optional): Number of variations per weight in batch
         
+        Returns:
+            torch.tensor: Tensor of augmentations
+        """
+        mask = (weight > self._cutoff).type(torch.int).float()
+        augmented_weights = weight.repeat(num_augmentations, 1)
+        augmented_mask = mask.repeat(num_augmentations, 1)
+        augmented_noise = augmentation_variance*augmented_mask*torch.rand_like(augmented_mask)
+        augmented_weights = torch.max(torch.zeros_like(augmented_weights), augmented_weights + augmented_noise)
+        return self.__normalize(augmented_weights)
+
+    def get_random_augmentations(self, weight, num_augmentations=5, prop_weights_affected=6./72., max_noise=0.3):
+        """Apply random weight upscaling to a proportion of prop_weights_affected weights with a uniform
+        value from 0 to max_noise.
+
+        Args:
+            weight (torch.tensor(batch,num_sigs)): Batch of guessed weights
+            num_augmentations (int, optional): Number of variations per weight in batch
+            prop_weights_affected (float, optional): Provability of changing a weight. Defaults to 6./72..
+            max_noise (float, optional): Maximum weight upscaling. Defaults to 0.3.
+
+        Returns:
+            torch.tensor: Tensor of augmentations
+        """
+        augmented_weights = weight.repeat(num_augmentations, 1)
+        mask = (torch.rand(augmented_weights.size()) > 1 - prop_weights_affected)
+        noise = max_noise*torch.rand(augmented_weights.size())*mask
+        augmented_weights = augmented_weights + noise
+        return self.__normalize(augmented_weights)
+
 if __name__== "__main__":
-    # signatures = read_signatures("../../data/data.xlsx")
-    signatures = [
-        torch.tensor([1, 0], dtype=torch.float),
-        torch.tensor([0, 1], dtype=torch.float),
-        torch.tensor([1, 1], dtype=torch.float),
-    ]
-
-    mutation_vect = torch.tensor([2.1, 1.0], dtype=torch.float)
-    inferred_weight = torch.tensor([1.0, 0.0, 1.0], dtype=torch.float)
-
-    """
-    In this case, we ideally want to obtain things around the line:
-    [2.1, 1, 0] + lambda*[1, 1, -1]
-    """
+    weight_batch = torch.tensor([[0.1, 0.9, 0], [0.3, 0.3, 0.4]])
     
-    weight_augmenter = WeightAugmenter(signatures)
-    augmentations = weight_augmenter.get_augmentations(
-        mutation_vect=mutation_vect,
-        inferred_weight=inferred_weight,
-        num_augmentations=5,
-        step_range=(0.0, 0.2)
+    weight_augmenter = WeightAugmenter()
+    augmentations = weight_augmenter.get_reweighted_augmentations(
+        weight=weight_batch,
+        num_augmentations=2,
     )
+    print(augmentations)
+    
+    augmentations = weight_augmenter.get_random_augmentations(
+        weight=weight_batch,
+        num_augmentations=2,
+        prop_weights_affected=1./4.
+    )
+    print(augmentations)
