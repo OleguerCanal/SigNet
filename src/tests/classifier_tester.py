181d4c8b59631d4f736b13d19d1049da63acbcea
cserranocolome <claudiaserranocolome@gmail.com>
2021-09-30 15:53:04 +0200

    Solved bug

--- src/tests/classifier_tester.py
+++ src/tests/classifier_tester.py
@@ -5,51 +5,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from models.classifier import Classifier
+from utilities.plotting import plot_metric_vs_mutations
 from utilities.io import csv_to_tensor
 
-def accuracy(prediction, label):
-    threshold = 0.5
-    prediction = (prediction>threshold).float()*1
-    return torch.sum(prediction == label)/torch.numel(prediction)*100
-
-def false_realistic(prediction, label):
-    threshold = 0.5
-    prediction = (prediction>threshold).float()*1
-    return torch.sum(label[prediction == 1] == 0)/torch.numel(label[prediction == 1])*100
-
-def false_random(prediction, label):
-    threshold = 0.5
-    prediction = (prediction>threshold).float()*1
-    return torch.sum(label[prediction == 0] == 1)/torch.numel(label[prediction == 0])*100
-
-def plot_metric_vs_mutations(guess, label, num_muts_list, plot_path = '../../plots/plot_classifier.png'):
-    fig, axs = plt.subplots(3)
-    fig.suptitle("Metrics vs Number of Mutations")
-    
-    num_muts_list = num_muts_list[num_muts_list<=100000]
-    num_muts = np.unique(num_muts_list.detach().numpy())
-    
-    values = np.zeros((3, len(num_muts)))
-    for i in range(len(num_muts)):
-        values[0,i] = accuracy(label=label[2000*i:2000*(i+1),:], prediction=guess[2000*i:2000*(i+1),:])
-        values[1,i] = false_realistic(label=label[2000*i:2000*(i+1),:], prediction=guess[2000*i:2000*(i+1),:])
-        values[2,i] = false_random(label=label[2000*i:2000*(i+1),:], prediction=guess[2000*i:2000*(i+1),:])
-        
-    axs[0].plot(np.log10(num_muts), values[0,:])
-    axs[0].set_ylabel("Accuracy (%)")
-
-    axs[1].plot(np.log10(num_muts), values[1,:])
-    axs[1].set_ylabel("False Realistic (%)")
-
-    axs[2].plot(np.log10(num_muts), values[2,:])
-    axs[2].set_ylabel("False Random (%)")
-
-    manager = plt.get_current_fig_manager()
-    manager.resize(*manager.window.maxsize())
-    plt.show()
-    #fig.savefig(plot_path)
 
 experiment_id = "exp_classifier"
 
