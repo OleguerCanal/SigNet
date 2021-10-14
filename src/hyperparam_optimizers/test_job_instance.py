b9d26b148ecdcbd32a7bfa91d8da74753aa47c79
OleguerCanal <oleguer.canal@hotmail.com>
2021-10-14 16:00:49 +0200

    Testing stuff

--- src/hyperparam_optimizers/test_job_instance.py
+++ src/hyperparam_optimizers/test_job_instance.py
@@ -2,7 +2,7 @@ import os
 import sys
 import subprocess
 import random
-
+import shlex
 import time
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -14,37 +14,47 @@ class TestJobInstance(SearchJobInstance):
         super().__init__(id)
 
     def launch(self,
-               batch_size,
-               lr,
-               num_hidden_layers,
-               num_units,
+               param=123,
                plot=False):
         self.passed_args = locals()
 
         # ssh into server and write random number on a file
-        command = 'ssh oleguer@192.168.0.19 "'
-        # command += 'sleep $[ ( $RANDOM % 10 )  + 1 ]s && '
-        command += 'echo ' + str(random.random()) + ' >> test/' + str(self.id) + ';"'
+        command = 'ssh -p 1969 oleguer@62.57.177.212 "'
+        command += 'python a.py"'
+        print("sending", command)
         self.process = subprocess.Popen(command, shell=True)
+        self.process.wait()
+        print(self.process)
+        print('returncode:', self.process.returncode)
 
     def get_result(self):
-        import shlex
-        command = "ssh oleguer@192.168.0.19 "
+        command = "ssh  -p 1969 oleguer@62.57.177.212 "
         command += "cat test/" + str(self.id)
         output = float(subprocess.check_output(shlex.split(command)).decode('utf-8').split("\n")[0])
         return output
 
     def done(self):
-        import shlex
-        command = "ssh oleguer@192.168.0.19 "
+        print(self.process.returncode)
+        if self.process.returncode is not None:
+            return False
+        command = "ssh  -p 1969 oleguer@62.57.177.212 "
         command += "ls test;"
         output = subprocess.check_output(shlex.split(command)).decode('utf-8').split("\n")
         is_done = str(self.id) in output
         return is_done
 
     def kill(self):
-        os.killpg(os.getpgid(self.process.pid), signal.SIGTERM)
+        self
 
     def end(self):
         pass
     
+if __name__ == "__main__":
+    test_job_instance = TestJobInstance(0)
+    test_job_instance.launch()
+    time.sleep(1)
+    print("Done: ", test_job_instance.done())
+    time.sleep(4)
+    print("Done: ", test_job_instance.done())
+    time.sleep(4)
+    print("Done: ", test_job_instance.done())
