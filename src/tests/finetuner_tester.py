25ee6968737d1ba487b33c5afa101b0ca8cd1b6e
cserranocolome <claudiaserranocolome@gmail.com>
2021-10-25 10:41:36 +0200

    adapt for v2

--- src/tests/finetuner_tester.py
+++ src/tests/finetuner_tester.py
@@ -3,7 +3,7 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from models.baseline import Baseline
-from utilities.io import read_signatures, read_test_data, read_model
+from utilities.io import read_methods_guesses, read_signatures, read_test_data, read_model
 from utilities.plotting import plot_metric_vs_mutations, plot_metric_vs_sigs
 
 experiment_id = "exp_final"
@@ -26,15 +26,20 @@ finetuner_guess_01 = finetuner(mutation_dist=input_batch,
 
 
 # Plot results
-list_of_methods = ['baseline', 'finetuner 0.1']
+# list_of_methods = ["decompTumor2Sig", "MutationalPatterns", "mutSignatures", "SignatureEstimationQP","YAPSA"]#, "deconstructSigs"]
+# list_of_guesses, label = read_methods_guesses('cpu', experiment_id, "test", list_of_methods, data_folder="../../data")
+
+list_of_methods = ['baseline', 'finetuner']
 list_of_guesses = [baseline_guess, finetuner_guess_01]
 
-list_of_metrics = ["MAE_p", "MAE_n", "fp", "fn"]
+plot_all_metrics_vs_mutations( list_of_methods, list_of_guesses, label_batch, '')
 
-plot_metric_vs_sigs(list_of_metrics, list_of_methods, list_of_guesses, label_batch, "../../plots/%s/metrics_vs_sigs.png"%experiment_id)
-plot_metric_vs_mutations(list_of_metrics, list_of_methods, list_of_guesses, label_batch, "../../plots/%s/metric_vs_mutations.png"%experiment_id)
+# list_of_metrics = ["MAE_p", "MAE_n", "fp", "fn"]
 
-list_of_metrics = ["accuracy %", "sens: tp/p %", "spec: tn/n %"]
+# plot_metric_vs_sigs(list_of_metrics, list_of_methods, list_of_guesses, label_batch, "../../plots/%s/metrics_vs_sigs.png"%experiment_id)
+# plot_metric_vs_mutations(list_of_metrics, list_of_methods, list_of_guesses, label_batch, "../../plots/%s/metric_vs_mutations.png"%experiment_id)
 
-plot_metric_vs_sigs(list_of_metrics, list_of_methods, list_of_guesses, label_batch, "../../plots/%s/metrics_acc_vs_sigs.png"%experiment_id)
-plot_metric_vs_mutations(list_of_metrics, list_of_methods, list_of_guesses, label_batch, "../../plots/%s/metric_acc_vs_mutations.png"%experiment_id)
+# list_of_metrics = ["accuracy %", "sens: tp/p %", "spec: tn/n %"]
+
+# plot_metric_vs_sigs(list_of_metrics, list_of_methods, list_of_guesses, label_batch, "../../plots/%s/metrics_acc_vs_sigs.png"%experiment_id)
+# plot_metric_vs_mutations(list_of_metrics, list_of_methods, list_of_guesses, label_batch, "../../plots/%s/metric_acc_vs_mutations.png"%experiment_id)
