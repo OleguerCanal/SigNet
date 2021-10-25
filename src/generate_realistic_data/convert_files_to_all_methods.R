ee350ec60ce25eb32c0d5b388340c1f37eb847b7
cserranocolome <claudiaserranocolome@gmail.com>
2021-10-25 10:41:08 +0200

    solved bugs

--- src/generate_realistic_data/convert_files_to_all_methods.R
+++ src/generate_realistic_data/convert_files_to_all_methods.R
@@ -5,11 +5,13 @@
 dataset <- "test"
 
 catalog <- read.csv(paste("PCAWG_output/", dataset, "/ground.truth.syn.catalog.csv", sep = ""))
-test_input_muts <- read.csv(paste("PCAWG_output/", name_dataset,  "_input_not_normalized.csv", sep = ""), col.names = FALSE, row.names = FALSE, sep = ",") 
+test_input <- read.csv(paste("../../data/exp_v2/test/test_input.csv", sep = ""), header = FALSE, row.names = NULL, sep = ",") 
+test_label <- read.csv(paste("../../data/exp_v2/test/test_label.csv", sep = ""), header = FALSE, row.names = NULL, sep = ",") 
+test_input_muts <- sapply(test_input, '*', test_label$V31)
 test_input_muts <- t(test_input_muts)
 test_input_muts <- cbind(catalog[,c(1,2)], test_input_muts)
-colnames(test_input_muts) <- colnames(catalog)
-if(dir.exists("PCAWG_output/other_methods/")==FALSE){
-  dir.create("PCAWG_output/other_methods/")
+colnames(test_input_muts) <- c(colnames(catalog)[c(1,2)], paste("sample_", 1:nrow(test_input), sep = ""))
+if(dir.exists("../../data/exp_v2/other_methods/")==FALSE){
+  dir.create("../../data/exp_v2/other_methods/")
 }
-write.table(test_input_muts, paste("PCAWG_output/other_methods/", dataset, "_ground.truth.syn.catalog.csv", sep = ""), row.names = FALSE, sep = ",") 
+write.table(test_input_muts, paste("../../data/exp_v2/other_methods/", dataset, "_ground.truth.syn.catalog.csv", sep = ""), row.names = FALSE, sep = ",") 
