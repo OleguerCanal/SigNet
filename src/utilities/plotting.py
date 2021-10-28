01e870cda745a958352c8958cf608afaeff42ca0
cserranocolome <claudiaserranocolome@gmail.com>
2021-10-28 19:58:49 +0200

    nice plots

--- src/utilities/plotting.py
+++ src/utilities/plotting.py
@@ -53,38 +53,39 @@ def plot_prop_signatures(weights_0, weights_augmented):
 
 # CLASSIFIER PLOTS:
 def plot_metric_vs_mutations_classifier(guess, label, num_muts_list, plot_path = '../../plots/exp_classifier/performance_classifier.png'):
-    fig, axs = plt.subplots(3)
+    fig, axs = plt.subplots(3, figsize=(8,6))
     fig.suptitle("Metrics vs Number of Mutations")
     
     num_muts_list = num_muts_list[num_muts_list<=100000]
     num_muts = np.unique(num_muts_list.detach().numpy())
     
+    marker_size = 3
+    line_width = 0.5
     values = np.zeros((3, len(num_muts)))
     for i in range(len(num_muts)):
         values[0,i] = accuracy(label=label[2000*i:2000*(i+1),:], prediction=guess[2000*i:2000*(i+1),:])
         values[1,i] = false_realistic(label=label[2000*i:2000*(i+1),:], prediction=guess[2000*i:2000*(i+1),:])
         values[2,i] = false_random(label=label[2000*i:2000*(i+1),:], prediction=guess[2000*i:2000*(i+1),:])
         
-    axs[0].plot(np.log10(num_muts), values[0,:])
-    axs[0].set_ylabel("Accuracy (%)")
+    axs[0].plot(np.log10(num_muts), values[0,:], marker='o',linewidth=line_width, markersize=marker_size)
 
-    axs[1].plot(np.log10(num_muts), values[1,:])
-    axs[1].set_ylabel("False Realistic (%)")
+    axs[1].plot(np.log10(num_muts), values[1,:], marker='o',linewidth=line_width, markersize=marker_size)
 
-    axs[2].plot(np.log10(num_muts), values[2,:])
-    axs[2].set_ylabel("False Random (%)")
+    axs[2].plot(np.log10(num_muts), values[2,:], marker='o',linewidth=line_width, markersize=marker_size)
 
-    manager = plt.get_current_fig_manager()
-    manager.resize(*manager.window.maxsize())
+    y_labels = ["Accuracy (%)", "False Realistic (%)", "False Random (%)"]
+    for i, axes in enumerate(axs.flat):
+        stylize_axes(axes, '', 'log(N)', y_labels[i])
+
     plt.show()
-    fig.savefig(plot_path)
+    # fig.savefig(plot_path)
     
 # FINETUNER PLOTS:
-def plot_all_metrics_vs_mutations(list_of_methods, list_of_guesses, label, plot_path):
+def plot_all_metrics_vs_mutations(list_of_methods, list_of_guesses, label, folder_path):
     '''
     Plot:
     MAE_p   MAE_n
-    FP      FN
+    FPR      FNR
     and in another plot
     Accuracy    Precision
     Sensitivity Specificity
@@ -92,7 +93,7 @@ def plot_all_metrics_vs_mutations(list_of_methods, list_of_guesses, label, plot_
     fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(8,6))
 
     num_muts = np.unique(label[:,-1].detach().numpy())
-    list_of_metrics = ["MAE_p", "MAE_n", "fp", "fn", "accuracy %", "precision %", "sens: tp/p %", "spec: tn/n %"]
+    list_of_metrics = ["MAE_p", "MAE_n", "fpr", "fnr", "accuracy %", "precision %", "sens: tp/p %", "spec: tn/n %"]
 
     values = np.zeros((len(list_of_methods), len(num_muts), len(list_of_metrics)))
     for method_index in range(len(list_of_methods)):
@@ -112,7 +113,7 @@ def plot_all_metrics_vs_mutations(list_of_methods, list_of_guesses, label, plot_
     axs[1,1].plot(np.log10(num_muts), np.transpose(values[:,:,3]), marker='o',linewidth=line_width, markersize=marker_size)
 
     xlabel = 'log(N)'
-    ylabel = ["MAE postives", "MAE negatives", "FP", "FN"]
+    ylabel = ["MAE postives", "MAE negatives", "FPR", "FNR"]
     fig.suptitle("Metrics vs Number of Mutations")
     for i, axes in enumerate(axs.flat):
         stylize_axes(axes, '', xlabel, ylabel[i])
@@ -122,8 +123,10 @@ def plot_all_metrics_vs_mutations(list_of_methods, list_of_guesses, label, plot_
     fig.legend(loc=7, labels=list_of_methods, prop={'size': 8})
     fig.tight_layout()
     fig.subplots_adjust(right=legend_adjustment)   
+    # create_dir(folder_path)
     plt.show()
-
+    # plt.savefig(folder_path + '/metrics_low.png')
+    plt.close()
     ############################################################################################
     
     fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(8,6))
@@ -147,6 +150,8 @@ def plot_all_metrics_vs_mutations(list_of_methods, list_of_guesses, label, plot_
     fig.tight_layout()
     fig.subplots_adjust(right=legend_adjustment)   
     plt.show()
+    # plt.savefig(folder_path + '/metrics_high.png')
+    plt.close()
 
     ############################################################################################
     mean_values = np.mean(values, axis=1)
@@ -167,11 +172,11 @@ def plot_all_metrics_vs_mutations(list_of_methods, list_of_guesses, label, plot_
     axs[1].set_xticks(np.array(range(len(list_of_metrics)))+width*len(list_of_methods)/2)
     axs[1].set_xticklabels(["MAE postives", "MAE negatives"])
 
-    list_of_metrics = ["fp", "fn"] 
+    list_of_metrics = ["fpr", "fnr"] 
     for method_index in range(len(list_of_methods)):
         axs[2].bar(np.array(range(len(list_of_metrics)))+width*method_index, mean_values[method_index,2:4], align='center', width=width)
     axs[2].set_xticks(np.array(range(len(list_of_metrics)))+width*len(list_of_methods)/2)
-    axs[2].set_xticklabels(["FP", "FN"])
+    axs[2].set_xticklabels(["FPR", "FNR"])
     
     for i, axes in enumerate(axs.flat):
         stylize_axes(axes, '', '', 'Metrics')
@@ -180,8 +185,8 @@ def plot_all_metrics_vs_mutations(list_of_methods, list_of_guesses, label, plot_
     fig.tight_layout()
     fig.subplots_adjust(right=legend_adjustment)   
     plt.show()
-    # # create_dir(plot_path)
-    # fig.savefig(plot_path)
+    # plt.savefig(folder_path + '/metrics_mean.png')
+    plt.close()
 
 
 def plot_metric_vs_mutations(list_of_metrics, list_of_methods, list_of_guesses, label, plot_path):
@@ -245,38 +250,33 @@ def plot_metric_vs_sigs(list_of_metrics, list_of_methods, list_of_guesses, label
 
 # ERRORLEARNER PLOTS:
 def plot_interval_metrics_vs_mutations(label, pred_upper, pred_lower, plot_path):
-    fig, axs = plt.subplots(2,2)
-    fig.suptitle("Interval metrics vs Number of Mutations")
+    fig, axs = plt.subplots(2,2, figsize=(8,6))
+    fig.suptitle("Interval Metrics vs Number of Mutations")
 
     num_muts = np.unique(label[:,-1].detach().numpy())
     values = np.zeros((4,len(num_muts)))
-    for i in range(len(num_muts)):
+    for i, num_mut in enumerate(num_muts):
         k = -1
-        metrics = get_pi_metrics(label[1000*i:1000*(i+1), :-1], pred_lower[1000*i:1000*(i+1), :], pred_upper[1000*i:1000*(i+1), :])
+        indexes = label[:, -1] == num_mut
+        metrics = get_pi_metrics(label[indexes, :-1], pred_lower[indexes, :], pred_upper[indexes, :])
         for metric in metrics.keys():
             k += 1
             values[k,i] = metrics[metric]
-        
-    axs[0,0].plot(np.log10(num_muts), values[0])
-    axs[0,0].set_ylabel("Proportion in")
-    axs[0,0].set_xlabel("log(N)")
+    marker_size = 3
+    line_width = 0.5
+    axs[0,0].plot(np.log10(num_muts), values[0], marker='o',linewidth=line_width, markersize=marker_size)
+    axs[0,1].plot(np.log10(num_muts), values[1], marker='o',linewidth=line_width, markersize=marker_size)
+    axs[1,0].plot(np.log10(num_muts), values[2], marker='o',linewidth=line_width, markersize=marker_size)
+    axs[1,1].plot(np.log10(num_muts), values[3], marker='o',linewidth=line_width, markersize=marker_size)
 
-    axs[0,1].plot(np.log10(num_muts), values[1])
-    axs[0,1].set_ylabel("Interval width")
-    axs[0,1].set_xlabel("log(N)")
+    xlabels = ["log(N)"]
+    ylabels = ["Proportion in (%)", "Interval Width", "Interval Width Present", "Interval Width Absent" ]
+    for i, axes in enumerate(axs.flat):
+        stylize_axes(axes, '', xlabels[0], ylabels[i])
 
-    axs[1,0].plot(np.log10(num_muts), values[2])
-    axs[1,0].set_ylabel("Interval width present")
-    axs[1,0].set_xlabel("log(N)")
-
-    axs[1,1].plot(np.log10(num_muts), values[3])
-    axs[1,1].set_ylabel("Interval width absent")
-    axs[1,1].set_xlabel("log(N)")
-
-    manager = plt.get_current_fig_manager()
-    manager.resize(*manager.window.maxsize())
+    fig.tight_layout()
     plt.show()
-    fig.savefig(plot_path)
+    # fig.savefig(plot_path)
 
 def plot_interval_metrics_vs_sigs(label, pred_upper, pred_lower, plot_path):
     fig, axs = plt.subplots(2,2)
@@ -309,25 +309,29 @@ def plot_interval_metrics_vs_sigs(label, pred_upper, pred_lower, plot_path):
     manager = plt.get_current_fig_manager()
     manager.resize(*manager.window.maxsize())
     plt.show()
-    fig.savefig(plot_path)
+    # fig.savefig(plot_path)
 
 def plot_interval_performance(label_batch, pred_upper, pred_lower, sigs_names, plot_path): # Returns x,y
+    label_batch = label_batch[:,:-1]
     lower = label_batch - pred_lower
     upper = pred_upper - label_batch
     num_error = torch.sum(lower<0, dim=0)
     num_error += torch.sum(upper<0, dim=0)
     num_error = num_error / label_batch.shape[0]
     num_classes = 72
-    fig, ax = plt.subplots(1,1)
+
+    fig, ax = plt.subplots(1,1, figsize=(12,10))
     fig.suptitle('Confidence intervals performance')
     ax.bar(range(num_classes), 100*num_error, align='center', width=0.2, alpha=0.5, ecolor='black', capsize=10)
     ax.set_ylabel("Percentage of error (%)")
+    
+    stylize_axes(ax, '', '', "Percentage of error (%)")
     ax.set_xticks(range(num_classes))
     ax.set_xticklabels(sigs_names, rotation='vertical')
-    manager = plt.get_current_fig_manager()
-    manager.resize(*manager.window.maxsize())
+
+    fig.tight_layout()
     plt.show()
-    fig.savefig(plot_path)
+    # fig.savefig(plot_path)
     return range(num_classes), 100*num_error
 
 def plot_interval_width_vs_mutations(label, upper, lower, plot = True): # Returns x,y
