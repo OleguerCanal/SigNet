d67353d5bab0aa3fd3da1ac2e0ea3bf05ab3dc78
cserranocolome <claudiaserranocolome@gmail.com>
2021-12-17 15:23:22 +0100

    Changes to train again all networks

--- src/train.sh
+++ src/train.sh
@@ -1,9 +1,9 @@
-# python train_finetuner.py --config_file=configs/finetuner/finetuner_random_large.yaml;
-# python train_finetuner.py --config_file=configs/finetuner/finetuner_random_low.yaml;
-# python train_finetuner.py --config_file=configs/finetuner/finetuner_realistic_large.yaml;
-# python train_finetuner.py --config_file=configs/finetuner/finetuner_realistic_low.yaml;
 
-python train_errorfinder.py --config_file=configs/errorfinder/errorfinder_random_large.yaml;
-python train_errorfinder.py --config_file=configs/errorfinder/errorfinder_random_low.yaml;
-python train_errorfinder.py --config_file=configs/errorfinder/errorfinder_realistic_large.yaml;
-python train_errorfinder.py --config_file=configs/errorfinder/errorfinder_realistic_low.yaml;
+python train_classifier.py --config_file=configs/classifier/classifier.yaml;
+
+python train_finetuner.py --config_file=configs/finetuner/finetuner_random_large.yaml;
+python train_finetuner.py --config_file=configs/finetuner/finetuner_random_low.yaml;
+python train_finetuner.py --config_file=configs/finetuner/finetuner_realistic_large.yaml;
+python train_finetuner.py --config_file=configs/finetuner/finetuner_realistic_low.yaml;
+
+python train_errorfinder.py --config_file=configs/errorfinder/errorfinder.yaml;
