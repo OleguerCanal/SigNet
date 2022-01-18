62eaa0d0deaef1d50537521659c434fc1483945b
OleguerCanal <oleguer.canal@hotmail.com>
2022-01-18 12:35:48 +0100

    Created perturbed baselines

--- src/models/baseline.py
+++ src/models/baseline.py
@@ -65,8 +65,8 @@ if __name__ == "__main__":
 
     in_files = [
         # generator
-        "/exp_generator/train_generator_large_input.csv",
-        "/exp_generator/val_generator_large_input.csv",
+        "/exp_generator/train_perturbed_low_input.csv",
+        "/exp_generator/train_perturbed_large_input.csv",
 
         # tests
         # "/exp_generator/test_generator_low_input.csv",
