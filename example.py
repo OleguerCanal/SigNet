73e1f11bce35bb4e7733641915e4ae2e8f5d155d
OleguerCanal <oleguer.canal@hotmail.com>
2022-06-19 23:31:58 +0200

    Improved signet usage example

--- example.py
+++ example.py
@@ -6,14 +6,15 @@ from argparse import ArgumentParser
 import pandas as pd
 import torch
 
+from signet import DATA
 from signet.modules.signet_module import SigNet
-from signet import DATA as SIGNET_DATA
+from signet.utilities.io import csv_to_tensor, write_final_outputs
 
 def parse_args():
     parser = ArgumentParser()
     parser.add_argument(
-        '--input_data', action='store', nargs=1, type=str, required=True,
-        help=f'Path to the input data to be analyzed.'
+        '--input_data', action='store', nargs=1, type=str, required=False, default=DATA + "datasets/real_input.csv",
+        help=f'Path to the input data to be analyzed. By default it will use PCAWG dataset'
     )
 
     parser.add_argument(
@@ -27,7 +28,7 @@ def parse_args():
     )
 
     parser.add_argument(
-        '--figures', action='store', nargs=1, type=bool, required=False, default=[False],
+        '--figures', action='store', nargs=1, type=bool, required=False, default=[True],
         help=f'Boolean. Whether to compute plots for the output. Default: "False".'
     )
     
@@ -36,27 +37,32 @@ def parse_args():
 
 if __name__ == "__main__":
     # Parse command-line arguments
-    config = parse_args()
+    args = parse_args()
 
-    input_file_path = "data/datasets/real_input.csv"
-    output_path = "data/case_study_Hypoxia/new_SigNet/" 
-    opportunities = None
-    plot_figs = False
+    # Load signet
+    signet = SigNet(opportunities_name_or_path=args.normalization)
 
-    signet = SigNet(opportunities_name_or_path=opportunities, signatures_path="../../data/data.xlsx")
-
-    input_file = pd.read_csv(input_file_path, header=0, index_col=0, sep=',')
-    mutation_data = torch.tensor(input_file.values, dtype=torch.float)
-    print(mutation_data)
-    weight_guess, upper_bound, lower_bound, classification, normalized_input = signet(mutation_vec=mutation_data)
+    # Read and prepare data
+    inputs = csv_to_tensor(file=args.input_data,
+                           header=0,
+                           index_col=0)
+    
+    # Run signet
+    results = signet(mutation_vec=mutation_data)
 
     # Write final outputs
-    write_final_outputs(weight_guess, lower_bound, upper_bound, classification, 0, input_file, output_path)
-
+    write_final_outputs(weights=results["finetuner_guess"],
+                        lower_bound=results["error_lower"], 
+                        upper_bound=results["error_upper"],
+                        classification=results["classification"],
+                        reconstruction_error=0,
+                        input_file=args.input_data,
+                        output_path=args.output,
+                        name="")
 
     # Plot figures
     if plot_figs:
-        sig_names = list(pd.read_excel("../../data/data.xlsx").columns)[1:]
+        sig_names = list(pd.read_excel(DATA + "data.xlsx").columns)[1:]
         for i in range(weight_guess.shape[0]):
             plot_weights(weight_guess[i,:], upper_bound[i,:], lower_bound[i,:], sig_names, output_path + "/plots/plot_sample_%s.png"%str(i))
         # plot_reconstruction(normalized_input, signet.baseline_guess, signet.signatures, list(range(weight_guess.shape[0])), output_path + "/plots/baseline_reconstruction")
