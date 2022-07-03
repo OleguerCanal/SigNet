d27f1f28f18592360adbe83dd08af8b7ec9007cc
OleguerCanal <oleguer.canal@hotmail.com>
2022-07-03 20:39:51 +0200

    Added package version

--- signet/__init__.py
+++ signet/__init__.py
@@ -1,5 +1,7 @@
 import os
 
+__version__ = "0.0.0"
+
 ROOT = os.path.dirname(__file__)
 TRAINED_MODELS = os.path.join(os.path.dirname(__file__), "trained_models/")
 DATA = os.path.join(os.path.dirname(__file__), "data")
