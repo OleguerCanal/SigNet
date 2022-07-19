0cfaddda79dcbaf3183b028c6cf3eab01df6b59c
cserranocolome <claudiaserranocolome@gmail.com>
2022-07-19 17:02:42 +0200

    changes for benchmark

--- signet/generate_data/other_methods/run_all_methods.R
+++ signet/generate_data/other_methods/run_all_methods.R
@@ -16,35 +16,32 @@ cosmic_version = "v3"
 if(cosmic_version == "v3"){
     path_to_sigs = "../../../data/exp_generator/test_generator/other_methods/data.csv"
     
-  }else if(cosmic_version == "v2"){
-    path_to_sigs = "../../../data/exp_v2/test/other_methods/data_v2.csv"
-    
-  }else{
-    print("Error: not implemented for this version of COSMIC")
-    break
+}else if(cosmic_version == "v2"){
+  path_to_sigs = "../../../data/exp_v2/test/other_methods/data_v2.csv"
+  
+}else{
+  print("Error: not implemented for this version of COSMIC")
+  break
 }
 
-# #!/usr/bin/env Rscript
-# args = commandArgs(trailingOnly=TRUE)
-# method = args[1]
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+method <- args[1]
+dataset <- args[2]
+replicate <- args[3]
 
-method <- "all"
-dataset <- "test"
+# method <- "all"
+# dataset <- "test"
 experiment_id <- "exp_all"
 
-total_time_methods <- rep(0, 12)
-names(total_time_methods) <- c("decompTumor2Sig", "MutationalPatterns", "mutSignatures",
-                               "SignatureEstimationQP", "SignatureEstimationSA", "YAPSA", "deconstructSigs",
-                               "sigfit", "mSigAct", "signeR", "SignatureAnalyzer", "mutSpec")
-k <- 1  
 # Reffiting only methods:
 if(method == "decompTumor2Sig" || method == "all"){
   print("Running decompTumor2Sig")
   start.time <- Sys.time()
   RundecompTumor2SigAttributeOnly(
-    input.catalog = paste("../../../data/", experiment_id, "/other_methods/", dataset, "_ground.truth.syn.catalog.csv", sep = ""),
+    input.catalog = paste("../../../data/", experiment_id, "/other_methods/test_ground.truth.syn.catalog_",k,".csv", sep = ""),
     gt.sigs.file = path_to_sigs,
-    paste("../../../data/", experiment_id, "/other_methods_time/decompTumor2Sig_test", sep=""),
+    paste("../../../data/", experiment_id, "/other_methods/decompTumor2Sig_test", sep=""),
     seedNumber = 1,
     test.only = FALSE,
     overwrite = FALSE
@@ -53,18 +50,14 @@ if(method == "decompTumor2Sig" || method == "all"){
   time.taken <- end.time - start.time
   print(time.taken)
 }
-
-total_time_methods[k] <- difftime(end.time, start.time, units='mins') 
-k <- k + 1
-write.table(total_time_methods, paste("../../../data/", experiment_id, "/other_methods_time/time_methods.txt", sep=""), sep='\t', col.names = FALSE)
 
 if(method == "MutationalPatterns" || method == "all"){
   print("Running MutationalPatterns")
   start.time <- Sys.time() 
   RunMutationalPatternsAttributeOnly(
-    input.catalog = paste("../../../data/", experiment_id, "/other_methods/", dataset, "_ground.truth.syn.catalog.csv", sep = ""),
+    input.catalog = paste("../../../data/", experiment_id, "/other_methods/test_ground.truth.syn.catalog_",k,".csv", sep = ""),
     gt.sigs.file = path_to_sigs,
-    paste("../../../data/", experiment_id, "/other_methods_time/MutationalPatterns_test", sep=""),
+    paste("../../../data/", experiment_id, "/other_methods/MutationalPatterns_test", sep=""),
     seedNumber = 1,
     test.only = FALSE,
     overwrite = FALSE
@@ -73,18 +66,14 @@ if(method == "MutationalPatterns" || method == "all"){
   time.taken <- end.time - start.time
   print(time.taken)
 }
-
-total_time_methods[k] <- difftime(end.time, start.time, units='mins') 
-k <- k + 1
-write.table(total_time_methods, paste("../../../data/", experiment_id, "/other_methods_time/time_methods.txt", sep=""), sep='\t', col.names = FALSE)
 
 if(method == "mutSignatures" || method == "all"){
   print("Running mutSignatures")
   start.time <- Sys.time()
   RunmutSignaturesAttributeOnly(
-    input.catalog = paste("../../../data/", experiment_id, "/other_methods/", dataset, "_ground.truth.syn.catalog.csv", sep = ""),
+    input.catalog = paste("../../../data/", experiment_id, "/other_methods/test_ground.truth.syn.catalog_",k,".csv", sep = ""),
     gt.sigs.file = path_to_sigs,
-    paste("../../../data/", experiment_id, "/other_methods_time/mutSignatures_test", sep=""),
+    paste("../../../data/", experiment_id, "/other_methods/mutSignatures_test", sep=""),
     seedNumber = 1,
     test.only = FALSE,
     overwrite = FALSE
@@ -93,18 +82,14 @@ if(method == "mutSignatures" || method == "all"){
   time.taken <- end.time - start.time
   print(time.taken)
 }
-
-total_time_methods[k] <- difftime(end.time, start.time, units='mins') 
-k <- k + 1
-write.table(total_time_methods, paste("../../../data/", experiment_id, "/other_methods_time/time_methods.txt", sep=""), sep='\t', col.names = FALSE)
 
 if(method == "SignatureEstimationQP" || method == "all"){
   print("Running SignatureEstimationQP")
   start.time <- Sys.time()
   RunSignatureEstimationQPAttributeOnly(
-    input.catalog = paste("../../../data/", experiment_id, "/other_methods/", dataset, "_ground.truth.syn.catalog.csv", sep = ""),
+    input.catalog = paste("../../../data/", experiment_id, "/other_methods/test_ground.truth.syn.catalog_",k,".csv", sep = ""),
     gt.sigs.file = path_to_sigs,
-    paste("../../../data/", experiment_id, "/other_methods_time/SignatureEstimationQP_test", sep=""),
+    paste("../../../data/", experiment_id, "/other_methods/SignatureEstimationQP_test", sep=""),
     seedNumber = 1,
     test.only = FALSE,
     overwrite = FALSE
@@ -113,18 +98,14 @@ if(method == "SignatureEstimationQP" || method == "all"){
   time.taken <- end.time - start.time
   print(time.taken)
 }
-
-total_time_methods[k] <- difftime(end.time, start.time, units='mins') 
-k <- k + 1
-write.table(total_time_methods, paste("../../../data/", experiment_id, "/other_methods_time/time_methods.txt", sep=""), sep='\t', col.names = FALSE)
 
 if(method == "SignatureEstimationSA" || method == "all"){
   print("Running SignatureEstimationSA")
   start.time <- Sys.time()
   RunSignatureEstimationSAAttributeOnly(
-    input.catalog = paste("../../../data/", experiment_id, "/other_methods/", dataset, "_ground.truth.syn.catalog.csv", sep = ""),
+    input.catalog = paste("../../../data/", experiment_id, "/other_methods/test_ground.truth.syn.catalog_",k,".csv", sep = ""),
     gt.sigs.file = path_to_sigs,
-    paste("../../../data/", experiment_id, "/other_methods_time/SignatureEstimationSA_test", sep=""),
+    paste("../../../data/", experiment_id, "/other_methods/SignatureEstimationSA_test", sep=""),
     seedNumber = 1,
     test.only = FALSE,
     overwrite = FALSE
@@ -134,17 +115,13 @@ if(method == "SignatureEstimationSA" || method == "all"){
   print(time.taken)
 }
 
-total_time_methods[k] <- difftime(end.time, start.time, units='mins') 
-k <- k + 1
-write.table(total_time_methods, paste("../../../data/", experiment_id, "/other_methods_time/time_methods.txt", sep=""), sep='\t', col.names = FALSE)
-
 if(method == "YAPSA" || method == "all"){
   print("Running YAPSA")
   start.time <- Sys.time()
   RunYAPSAAttributeOnly(
-    input.catalog = paste("../../../data/", experiment_id, "/other_methods/", dataset, "_ground.truth.syn.catalog.csv", sep = ""),
+    input.catalog = paste("../../../data/", experiment_id, "/other_methods/test_ground.truth.syn.catalog_",k,".csv", sep = ""),
     gt.sigs.file = path_to_sigs,
-    paste("../../../data/", experiment_id, "/other_methods_time/YAPSA_test", sep=""),
+    paste("../../../data/", experiment_id, "/other_methods/YAPSA_test", sep=""),
     seedNumber = 1,
     signature.cutoff = NULL,
     test.only = FALSE,
@@ -155,17 +132,13 @@ if(method == "YAPSA" || method == "all"){
   print(time.taken)
 }
 
-total_time_methods[k] <- difftime(end.time, start.time, units='mins') 
-k <- k + 1
-write.table(total_time_methods, paste("../../../data/", experiment_id, "/other_methods_time/time_methods.txt", sep=""), sep='\t', col.names = FALSE)
-
 if(method == "deconstructSigs" || method == "all"){
   print("Running deconstructSigs")
   start.time <- Sys.time()
   RundeconstructSigsAttributeOnly(
-    input.catalog = paste("../../../data/", experiment_id, "/other_methods/", dataset, "_ground.truth.syn.catalog.csv", sep = ""),
+    input.catalog = paste("../../../data/", experiment_id, "/other_methods/test_ground.truth.syn.catalog_",k,".csv", sep = ""),
     gt.sigs.file = path_to_sigs,
-    paste("../../../data/", experiment_id, "/other_methods_time/deconstructSigs_test", sep=""),
+    paste("../../../data/", experiment_id, "/other_methods/deconstructSigs_test", sep=""),
     seedNumber = 1,
     test.only = FALSE,
     overwrite = FALSE
@@ -174,18 +147,14 @@ if(method == "deconstructSigs" || method == "all"){
   time.taken <- end.time - start.time
   print(time.taken)
 }
-
-total_time_methods[k] <- difftime(end.time, start.time, units='mins') 
-k <- k + 1
-write.table(total_time_methods, paste("../../../data/", experiment_id, "/other_methods_time/time_methods.txt", sep=""), sep='\t', col.names = FALSE)
 
 if(method == "sigfit" || method == "all"){
   print("Running sigfit")
   start.time <- Sys.time()
   RunsigfitAttributeOnly(
-    input.catalog = paste("../../../data/", experiment_id, "/other_methods/", dataset, "_ground.truth.syn.catalog.csv", sep = ""),
+    input.catalog = paste("../../../data/", experiment_id, "/other_methods/test_ground.truth.syn.catalog_",k,".csv", sep = ""),
     gt.sigs.file = path_to_sigs,
-    paste("../../../data/", experiment_id, "/other_methods_time/sigfit_test", sep=""),
+    paste("../../../data/", experiment_id, "/other_methods/sigfit_test", sep=""),
     seedNumber = 1,
     test.only = FALSE,
     overwrite = FALSE
@@ -194,18 +163,14 @@ if(method == "sigfit" || method == "all"){
   time.taken <- end.time - start.time
   print(time.taken)
 }
-
-total_time_methods[k] <- difftime(end.time, start.time, units='mins') 
-k <- k + 1
-write.table(total_time_methods, paste("../../../data/", experiment_id, "/other_methods_time/time_methods.txt", sep=""), sep='\t', col.names = FALSE)
 
 if(method == "mSigAct" || method == "all"){
   print("Running mSigAct")
   start.time <- Sys.time() 
   RunmSigActAttributeOnly(
-    input.catalog = paste("../../../data/", experiment_id, "/other_methods/", dataset, "_ground.truth.syn.catalog.csv", sep = ""),
+    input.catalog = paste("../../../data/", experiment_id, "/other_methods/test_ground.truth.syn.catalog_",k,".csv", sep = ""),
     gt.sigs.file = path_to_sigs,
-    paste("../../../data/", experiment_id, "/other_methods_time/mSigAct_test", sep=""),
+    paste("../../../data/", experiment_id, "/other_methods/mSigAct_test", sep=""),
     seedNumber = 1,
     test.only = FALSE,
     overwrite = FALSE
@@ -214,10 +179,6 @@ if(method == "mSigAct" || method == "all"){
   time.taken <- end.time - start.time
   print(time.taken)
 }
-
-total_time_methods[k] <- difftime(end.time, start.time, units='mins') 
-k <- k + 1
-write.table(total_time_methods, paste("../../../data/", experiment_id, "/other_methods_time/time_methods.txt", sep=""), sep='\t', col.names = FALSE)
 
 
 # Extraction + reffiting methods:
@@ -226,9 +187,9 @@ if(method == "signeR" || method == "all"){
   print("Running signeR")
   start.time <- Sys.time()
   RunsigneR(
-    input.catalog = paste("../../../data/", experiment_id, "/other_methods/", dataset, "_ground.truth.syn.catalog.csv", sep = ""),
+    input.catalog = paste("../../../data/", experiment_id, "/other_methods/test_ground.truth.syn.catalog_",k,".csv", sep = ""),
     gt.sigs.file = path_to_sigs,
-    paste("../../../data/", experiment_id, "/other_methods_time/signeR_test", sep=""),
+    paste("../../../data/", experiment_id, "/other_methods/signeR_test", sep=""),
     seedNumber = 1,
     test.only = FALSE,
     overwrite = FALSE,
@@ -239,17 +200,13 @@ if(method == "signeR" || method == "all"){
   print(time.taken)
 }
 
-total_time_methods[k] <- difftime(end.time, start.time, units='mins') 
-k <- k + 1
-write.table(total_time_methods, paste("../../../data/", experiment_id, "/other_methods_time/time_methods.txt", sep=""), sep='\t', col.names = FALSE)
-
 if(method == "SignatureAnalyzer" || method == "all"){
   print("Running SignatureAnalyzer")
   start.time <- Sys.time()
   RunSignatureAnalyzerAttribution(
-    input.catalog = paste("../../../data/", experiment_id, "/other_methods/", dataset, "_ground.truth.syn.catalog.csv", sep = ""),
+    input.catalog = paste("../../../data/", experiment_id, "/other_methods/test_ground.truth.syn.catalog_",k,".csv", sep = ""),
     gt.sigs.file = path_to_sigs,
-    paste("../../../data/", experiment_id, "/other_methods_time/SignatureAnalyzer_test", sep=""),
+    paste("../../../data/", experiment_id, "/other_methods/SignatureAnalyzer_test", sep=""),
     seedNumber = 1,
     test.only = FALSE,
     overwrite = FALSE
@@ -259,17 +216,13 @@ if(method == "SignatureAnalyzer" || method == "all"){
   print(time.taken)
 }
 
-total_time_methods[k] <- difftime(end.time, start.time, units='mins') 
-k <- k + 1
-write.table(total_time_methods, paste("../../../data/", experiment_id, "/other_methods_time/time_methods.txt", sep=""), sep='\t', col.names = FALSE)
-
 if(method == "mutSpec" || method == "all"){
   print("Running mutSpec")
   start.time <- Sys.time()
   RunmutSpec(
-    input.catalog = paste("../../../data/", experiment_id, "/other_methods/", dataset, "_ground.truth.syn.catalog.csv", sep = ""),
+    input.catalog = paste("../../../data/", experiment_id, "/other_methods/test_ground.truth.syn.catalog_",k,".csv", sep = ""),
     gt.sigs.file = path_to_sigs,
-    paste("../../../data/", experiment_id, "/other_methods_time/mutSpec_test", sep=""),
+    paste("../../../data/", experiment_id, "/other_methods/mutSpec_test", sep=""),
     seedNumber = 1,
     test.only = FALSE,
     overwrite = FALSE,
@@ -280,6 +233,4 @@ if(method == "mutSpec" || method == "all"){
   print(time.taken)
 }
 
-total_time_methods[k] <- difftime(end.time, start.time, units='mins') 
-k <- k + 1
-write.table(total_time_methods, paste("../../../data/", experiment_id, "/other_methods_time/time_methods.txt", sep=""), sep='\t', col.names = FALSE)
+write.table(difftime(end.time, start.time, units='mins'), paste("../../../data/", experiment_id, "/other_methods/times/", method, dataset,"_", replicate, ".txt",sep=""), sep='\t', col.names = FALSE)
