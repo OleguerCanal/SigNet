515f2e651dfcac02735a86539b26912d3d88d11f
cserranocolome <claudiaserranocolome@gmail.com>
2022-07-21 18:14:18 +0200

    finetuner tester with computation time

--- signet/tests/finetuner_tester.py
+++ signet/tests/finetuner_tester.py
@@ -4,6 +4,8 @@ import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import time
 import torch
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
@@ -20,145 +22,154 @@ experiment_id = "crossval"
 
 # Plots crossvalidation
 
-# k_tot = 10
+k_tot = 10
 
-# # Create partitions
-# lst_weights, lst_ctype = read_data_and_partitions(k_tot)
+# Create partitions
+lst_weights, lst_ctype = read_data_and_partitions(k_tot)
 
-# # Oversample each set to have the same number of samples for each cancer type
-# oversampled_weights = []
-# for i in range(k_tot):
-#     oversampler = CancerTypeOverSampler(lst_weights[i], lst_ctype[i])
-#     oversampled_weights.append(oversampler.get_even_set())
+# Oversample each set to have the same number of samples for each cancer type
+oversampled_weights = []
+for i in range(k_tot):
+    oversampler = CancerTypeOverSampler(lst_weights[i], lst_ctype[i])
+    oversampled_weights.append(oversampler.get_even_set())
 
-# # Create inputs associated to the labels:
-# signatures = read_signatures(
-#     "../../data/data.xlsx", mutation_type_order="../../data/mutation_type_order.xlsx")
-# data_generator = DataGenerator(signatures=signatures,
-#                                 seed=None,
-#                                 shuffle=True)
+# Create inputs associated to the labels:
+signatures = read_signatures(
+    "../../data/data.xlsx", mutation_type_order="../../data/mutation_type_order.xlsx")
+data_generator = DataGenerator(signatures=signatures,
+                                seed=None,
+                                shuffle=True)
 
-# models_path = "../../trained_models/" + experiment_id
+models_path = "../../trained_models/" + experiment_id
 
-# # Loop through the partitions
-# list_of_metrics = ["MAE", "KL", "fpr", "fnr", "accuracy %",
-#                     "precision %", "sens: tp/p %", "spec: tn/n %"]
-# num_muts = [25, 50, 100, 250, 500, 1e3, 5e3, 1e4, 5e4, 1e5]
-# values = np.zeros((k_tot, len(num_muts), len(list_of_metrics)))
-# for k in range(k_tot):
-#     current_test = i
+# Loop through the partitions
+list_of_metrics = ["MAE", "KL", "fpr", "fnr", "accuracy %",
+                    "precision %", "sens: tp/p %", "spec: tn/n %"]
+num_muts = [25, 50, 100, 250, 500, 1e3, 5e3, 1e4, 5e4, 1e5]
+values = np.zeros((k_tot, len(num_muts), len(list_of_metrics)))
+times = np.zeros((k_tot, len(num_muts)))
+for k in range(k_tot):
+    current_test = i
 
-#     # Create test weight sets
-#     test_weights = oversampled_weights[current_test]
+    # Create test weight sets
+    test_weights = oversampled_weights[current_test]
 
-#     # Create pairs input-label
-#     test_input, test_label = data_generator.make_input(test_weights, "test", "", normalize=True)
-
-#     # Run Baseline
-#     print("Running Baseline")
-#     sf = Baseline(signatures)
-#     test_baseline = sf.get_weights_batch(test_input, n_workers=4)
-
-#     # Apply model to test set
-#     finetuner = CombinedFinetuner(low_mum_mut_dir=models_path + "/finetuner_low_crossval_" + str(i),
-#                                     large_mum_mut_dir=models_path + "/finetuner_large_crossval_" + str(i))
-#     finetuner_guess = finetuner(mutation_dist=test_input,
-#                                 baseline_guess=test_baseline,
-#                                 num_mut=test_label[:, -1].view(-1, 1))
-
-#     # Test model
+    # Create pairs input-label
+    test_input, test_label = data_generator.make_input(test_weights, "test", "", normalize=True)
     
-#     for i, num_mut in enumerate(num_muts):
-#         indexes = test_label[:, -1] == num_mut
-#         metrics = get_classification_metrics(label_batch=test_label[indexes, :-1],
-#                                                 prediction_batch=finetuner_guess[indexes,:])
-#         for metric_index, metric in enumerate(list_of_metrics):
-#             values[k, i, metric_index] = metrics[metric]
+    for i, num_mut in enumerate(num_muts):
+        indexes = test_label[:, -1] == num_mut
 
-# # Plot final results
-# plot_crossval(values, num_muts)
+        st = time.time()
 
-# # Compute final metrics crossvalidation and benchmark:
+        # Run Baseline
+        print("Running Baseline")
+        sf = Baseline(signatures)
+        test_baseline = sf.get_weights_batch(test_input[indexes,:], n_workers=4)
 
-# # Load Baseline and get guess
-# baseline = Baseline(signatures)
-# baseline_guess = baseline.get_weights_batch(test_input, n_workers=2)
+        # Apply model to test set
+        finetuner = CombinedFinetuner(low_mum_mut_dir=models_path + "/finetuner_low_crossval_" + str(i),
+                                        large_mum_mut_dir=models_path + "/finetuner_large_crossval_" + str(i))
+        finetuner_guess = finetuner(mutation_dist=test_input[indexes,:],
+                                    baseline_guess=test_baseline,
+                                    num_mut=test_label[indexes, -1].view(-1, 1))
+        et = time.time()
+        times[k, i] = et-st
+        
+        # Test model
+        metrics = get_classification_metrics(label_batch=test_label[indexes, :-1],
+                                             prediction_batch=finetuner_guess)
+        for metric_index, metric in enumerate(list_of_metrics):
+            values[k, i, metric_index] = metrics[metric]
 
-# list_of_methods = ["decompTumor2Sig", "MutationalPatterns", "mutSignatures", "SignatureEstimationQP","YAPSA", "deconstructSigs"]
-# list_of_guesses, label = read_methods_guesses('cpu', "exp_all", list_of_methods, data_folder="../../data/")
-# # list_of_methods += ['NNLS', 'Finetuner']
-# # list_of_guesses += [baseline_guess, finetuner_guess]
+times_df = pd.DataFrame(times)
+times_df.columns = num_muts
+times_df.to_csv('SigNet_times.txt')
 
-# plot_crossval_benchmark(list_of_methods, list_of_guesses, label, values, folder_path='', show=True)
+# Plot final results
+plot_crossval(values, num_muts)
 
-# plot_all_metrics_vs_mutations(list_of_methods, list_of_guesses, test_label, '', show=True)
+# Compute final metrics crossvalidation and benchmark:
 
-# metrics_baseline = get_classification_metrics(label_batch=test_label[:, :-1],
-#                                      prediction_batch=list_of_guesses[0][:, :])
-# metrics_guess_1 = get_classification_metrics(label_batch=test_label[:, :-1],
-#                                      prediction_batch=list_of_guesses[1][:, :])
+# Load Baseline and get guess
+baseline = Baseline(signatures)
+baseline_guess = baseline.get_weights_batch(test_input, n_workers=2)
 
-# plot_weights_comparison(true_labels=metrics_baseline["MAE_sign"],
-#                         guessed_labels=metrics_guess_1["MAE_sign"], 
-#                         pred_upper=metrics_guess_1["MAE_sign"],
-#                         pred_lower=metrics_guess_1["MAE_sign"],
-#                         sigs_names=[str(v+1) for v in list(range(72))],
-#                         plot_path="")
-                        #labels={"true":"baseline", "guessed":"finetuner"})
+list_of_methods = ["decompTumor2Sig", "MutationalPatterns", "mutSignatures", "SignatureEstimationQP","YAPSA", "deconstructSigs"]
+list_of_guesses, label = read_methods_guesses('cpu', "exp_all", list_of_methods, data_folder="../../data/")
+# list_of_methods += ['NNLS', 'Finetuner']
+# list_of_guesses += [baseline_guess, finetuner_guess]
+
+plot_crossval_benchmark(list_of_methods, list_of_guesses, label, values, folder_path='', show=True)
+
+plot_all_metrics_vs_mutations(list_of_methods, list_of_guesses, test_label, '', show=True)
+
+metrics_baseline = get_classification_metrics(label_batch=test_label[:, :-1],
+                                     prediction_batch=list_of_guesses[0][:, :])
+metrics_guess_1 = get_classification_metrics(label_batch=test_label[:, :-1],
+                                     prediction_batch=list_of_guesses[1][:, :])
+
+plot_weights_comparison(true_labels=metrics_baseline["MAE_sign"],
+                        guessed_labels=metrics_guess_1["MAE_sign"], 
+                        pred_upper=metrics_guess_1["MAE_sign"],
+                        pred_lower=metrics_guess_1["MAE_sign"],
+                        sigs_names=[str(v+1) for v in list(range(72))],
+                        plot_path="",
+                        labels={"true":"baseline", "guessed":"finetuner"})
 # Without crossval:
 
 
-model_names = ["_fp_1", "_fp_01", "_fp_001", ""]
+# model_names = ["_fp_1", "_fp_01", "_fp_001", ""]
 
-for model_name in model_names:
-    input_batch = csv_to_tensor("../../data/exp_all/test_input.csv")
-    label_batch = csv_to_tensor("../../data/exp_all/test_label.csv")
+# for model_name in model_names:
+#     input_batch = csv_to_tensor("../../data/exp_all/test_input.csv")
+#     label_batch = csv_to_tensor("../../data/exp_all/test_label.csv")
 
-    signatures = read_signatures("../../data/data.xlsx")
-    # Run Baseline
-    print("Running Baseline")
-    sf = Baseline(signatures)
-    test_baseline = sf.get_weights_batch(input_batch, n_workers=4)
+#     signatures = read_signatures("../../data/data.xlsx")
+#     # Run Baseline
+#     print("Running Baseline")
+#     sf = Baseline(signatures)
+#     test_baseline = sf.get_weights_batch(input_batch, n_workers=4)
 
-    models_path = "../../trained_models/exp_fp"
-    finetuner = CombinedFinetuner(low_mum_mut_dir=models_path + "/finetuner_low_1" + model_name,
-                                    large_mum_mut_dir=models_path + "/finetuner_large_1" + model_name)
-    finetuner_guess = finetuner(mutation_dist=input_batch,
-                                baseline_guess=test_baseline,
-                                num_mut=label_batch[:, -1].view(-1, 1))
+#     models_path = "../../trained_models/exp_fp"
+#     finetuner = CombinedFinetuner(low_mum_mut_dir=models_path + "/finetuner_low_1" + model_name,
+#                                     large_mum_mut_dir=models_path + "/finetuner_large_1" + model_name)
+#     finetuner_guess = finetuner(mutation_dist=input_batch,
+#                                 baseline_guess=test_baseline,
+#                                 num_mut=label_batch[:, -1].view(-1, 1))
 
-    # list_of_methods = ["decompTumor2Sig", "MutationalPatterns", "mutSignatures", "SignatureEstimationQP","YAPSA"]#, "deconstructSigs"]
-    # list_of_guesses, label = read_methods_guesses('cpu', "exp_all", list_of_methods, data_folder="../../data/")
-    # list_of_methods += ['NNLS', 'Finetuner']
-    # list_of_guesses += [test_baseline, finetuner_guess]
+#     # list_of_methods = ["decompTumor2Sig", "MutationalPatterns", "mutSignatures", "SignatureEstimationQP","YAPSA"]#, "deconstructSigs"]
+#     # list_of_guesses, label = read_methods_guesses('cpu', "exp_all", list_of_methods, data_folder="../../data/")
+#     # list_of_methods += ['NNLS', 'Finetuner']
+#     # list_of_guesses += [test_baseline, finetuner_guess]
 
-    # plot_all_metrics_vs_mutations(list_of_methods, list_of_guesses, label_batch, '', show=True)
+#     # plot_all_metrics_vs_mutations(list_of_methods, list_of_guesses, label_batch, '', show=True)
 
-    guesses = finetuner_guess.detach().cpu().numpy()
-    labels = label_batch[:, :-1].detach().cpu().numpy()
+#     guesses = finetuner_guess.detach().cpu().numpy()
+#     labels = label_batch[:, :-1].detach().cpu().numpy()
 
-    from scipy.stats import kstest
-    import matplotlib.pyplot as plt
-    import numpy as np
+#     from scipy.stats import kstest
+#     import matplotlib.pyplot as plt
+#     import numpy as np
 
-    score_0 = kstest(guesses[:, 0], labels[:, 0])
-    score_2 = kstest(guesses[:, 2], labels[:, 2])
-    score_26 = kstest(guesses[:, 26], labels[:, 26])
-    score_48 = kstest(guesses[:, 48], labels[:, 48])
+#     score_0 = kstest(guesses[:, 0], labels[:, 0])
+#     score_2 = kstest(guesses[:, 2], labels[:, 2])
+#     score_26 = kstest(guesses[:, 26], labels[:, 26])
+#     score_48 = kstest(guesses[:, 48], labels[:, 48])
 
 
-    # plt.hist(guesses[:, 26], bins=50, alpha=0.5)
-    # plt.hist(labels[:, 26], bins=50, alpha=0.5)
-    # plt.show()
+#     # plt.hist(guesses[:, 26], bins=50, alpha=0.5)
+#     # plt.hist(labels[:, 26], bins=50, alpha=0.5)
+#     # plt.show()
 
-    # print("score_0", score_0)
-    # print("score_2", score_2)
-    # print("score_26", score_26)
-    # print("score_48", score_48)
+#     # print("score_0", score_0)
+#     # print("score_2", score_2)
+#     # print("score_26", score_26)
+#     # print("score_48", score_48)
 
-    scores = []
-    for i in range(guesses.shape[1]):
-        scores.append(kstest(guesses[:, i], labels[:, i])[0])
-    scores = np.sort(np.array(scores))
+#     scores = []
+#     for i in range(guesses.shape[1]):
+#         scores.append(kstest(guesses[:, i], labels[:, i])[0])
+#     scores = np.sort(np.array(scores))
 
-    print("Model:", model_name, np.mean(scores[int(scores.shape[0]*.75):]))
+#     print("Model:", model_name, np.mean(scores[int(scores.shape[0]*.75):]))
