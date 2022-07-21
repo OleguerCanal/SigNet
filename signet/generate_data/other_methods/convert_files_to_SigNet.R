0b493142f1ab558ce1806129dcf59dd86e3c4070
cserranocolome <claudiaserranocolome@gmail.com>
2022-07-21 15:17:03 +0200

    other methods results merge

--- signet/generate_data/other_methods/convert_files_to_SigNet.R
+++ signet/generate_data/other_methods/convert_files_to_SigNet.R
@@ -1,11 +1,13 @@
 # Convert output files from each method to SigNet format:
 
 experiment_id <- "exp_all"
-list_of_methods <- c("decompTumor2Sig", "mutSignatures", "SignatureEstimationQP", "YAPSA" ,"MutationalPatterns","deconstructSigs")
+list_of_methods <- c("decompTumor2Sig", "deconstructSigs", "mSigAct", "MutationalPatterns", "mutSignatures", "SignatureEstimationQP", "YAPSA" )
 
 for(i in 1:length(list_of_methods)){
-  test_guess <- read.csv(paste("../../../data/", experiment_id, "/other_methods/", list_of_methods[i], "_test/inferred.exposures.csv", sep=""), row.names = 1)
-  test_guess <- t(test_guess)
-  test_guess <- test_guess / rowSums(test_guess)
-  write.table(test_guess, paste("../../../data/", experiment_id, "/other_methods/", list_of_methods[i], "_guess.csv", sep = ""), col.names = FALSE, row.names = FALSE, sep = ",") 
+  final_guess <- c()
+  for(j in 1:10){
+    test_guess <- read.csv(paste("../../../data/", experiment_id, "/other_methods/", list_of_methods[i], "_test_", j,"/inferred.exposures.csv", sep=""), row.names = 1)
+    final_guess <- rbind(final_guess, t(test_guess))
+  } 
+  write.table(test_guess, paste("../../../data/", experiment_id, "/other_methods/all_results/", list_of_methods[i], "_guess.csv", sep = ""), col.names = FALSE, row.names = FALSE, sep = ",")
 }
